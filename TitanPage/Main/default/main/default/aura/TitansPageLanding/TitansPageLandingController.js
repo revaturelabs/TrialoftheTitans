ffd91a577ccee08593c61fb152685d737b5c97e6
Dominic Blasi <80599114+domjblasi@users.noreply.github.com>
2021-05-24 17:09:19 -0500

    5:09pm 05/24/21 Dominic

--- TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingController.js
+++ TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
+    cShowTitan : function(component, event, helper) {
+        helper.hShowTitan(component, event);
     }
 })
--- TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingHelper.js
+++ TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingHelper.js
@@ -1,5 +1,6 @@
 ({
-    helperMethod : function() {
-
+    hShowTitan : function(component, event, helper){
+        var showThisDiv = component.find("titanDiv1");
+        $A.util.removeClass(showThisDiv,"toggle-hide");
     }
 })
