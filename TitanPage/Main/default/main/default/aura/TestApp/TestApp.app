f7cd40f67fef277dce24eab89e3fbc60eb2592e2
Dominic Blasi <80599114+domjblasi@users.noreply.github.com>
2021-05-25 12:35:23 -0500

    12:35pm 05/25/21 Dominic

--- TitanPage/Main/default/main/default/aura/TestApp/TestApp.app
+++ TitanPage/Main/default/main/default/aura/TestApp/TestApp.app
@@ -2,17 +2,9 @@
     
     <!-- <aura:attribute name="" type=""/>  -->
     
+    <!-- Blank method  -->
     <lightning:button label="Titan 1" onclick="{!cTitanMethod}"/>
     
     <c:TitansPageLanding/>
-    <div class="slds-grid slds-gutters">
-    <div class="slds-col slds-size_6-of-12">
-    <c:ExamsLanding/>
-	<c:LandingSkills/>
-    </div>
 
-    <div class="slds-col slds-size_6-of-12">
-
-    </div>
-    </div>
 </aura:application>
--- TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingHelper.js
+++ TitanPage/Main/default/main/default/aura/TitansPageLanding/TitansPageLandingHelper.js
@@ -1,6 +1,80 @@
-({
-    hShowTitan : function(component, event, helper){
-        var showThisDiv = component.find("titanDiv1");
-        $A.util.removeClass(showThisDiv,"toggle-hide");
+({   
+    
+//	hShowTitan# : button method that fires event, chooses which Titan to display info for
+
+    hShowTitan1 : function(component, event, helper) {
+        var TitanPageLandingEvent = component.getEvent("TitanPageLandingEvent");
+        TitanPageLandingEvent.setParams({
+            "activeTitan" : "titanDiv1",
+        });
+        TitanPageLandingEvent.fire();
+        console.log("TitanPageLandingEvent fired: titanDiv1")
+    },
+    
+    hShowTitan2 : function(component, event, helper) {
+        var TitanPageLandingEvent = component.getEvent("TitanPageLandingEvent");
+        TitanPageLandingEvent.setParams({
+            "activeTitan" : "titanDiv2",
+        });
+        TitanPageLandingEvent.fire();
+        console.log("TitanPageLandingEvent fired: titanDiv2")
+    },
+    
+    hShowTitan3 : function(component, event, helper) {
+        var TitanPageLandingEvent = component.getEvent("TitanPageLandingEvent");
+        TitanPageLandingEvent.setParams({
+            "activeTitan" : "titanDiv3",
+        });
+        TitanPageLandingEvent.fire();
+        console.log("TitanPageLandingEvent fired: titanDiv3")
+    },
+    
+    hShowTitan4 : function(component, event, helper) {
+        var TitanPageLandingEvent = component.getEvent("TitanPageLandingEvent");
+        TitanPageLandingEvent.setParams({
+            "activeTitan" : "titanDiv4",
+        });
+        TitanPageLandingEvent.fire();
+        console.log("TitanPageLandingEvent fired: titanDiv4")
+    },
+    
+//	hNavigate : uses switch statement to show the Titan according to hShowTitan# method
+    
+    hNavigate : function(component, event, helper) {
+        console.log("hNavigate function");
+        var divList = [
+            component.find("titanDiv1"),
+            component.find("titanDiv2"),
+            component.find("titanDiv3"),
+            component.find("titanDiv4")
+        ];
+        
+        for(let i=0; i<divList.length; i++){
+            let displayDiv = divList[i];
+            $A.util.addClass(displayDiv,"toggle-hide");
+        };
+        
+        var navTo = event.getParam("activeTitan");
+        console.log("Event Received");
+        
+        switch (navTo){
+            case "titanDiv1":
+                var displayDiv = component.find("titanDiv1");
+                $A.util.removeClass(displayDiv,"toggle-hide");
+                break;
+            case "titanDiv2":
+                var displayDiv = component.find("titanDiv2");
+                $A.util.removeClass(displayDiv,"toggle-hide");
+                break;
+            case "titanDiv3":
+                var displayDiv = component.find("titanDiv3");
+                $A.util.removeClass(displayDiv,"toggle-hide");
+                break;
+            case "titanDiv4":
+                var displayDiv = component.find("titanDiv4");
+                $A.util.removeClass(displayDiv,"toggle-hide");
+                break;
+        }
     }
+    
 })
