d9f9857765f46b14a87b15c756da7b1969e84d3b
Josh Miccolo <65175521+Jmiccolo@users.noreply.github.com>
2021-05-25 16:09:06 -0400

    adding mock data and finishing up UserContext

--- HeroHubPackage/main/default/aura/PageContainer/PageContainerController.js
+++ HeroHubPackage/main/default/aura/PageContainer/PageContainerController.js
@@ -9,7 +9,7 @@
                 //User isn't signed in return to login
             }
         });*/
-        component.set("v.contextInfo", {id:"00273731279", name:"Gladius Maximus", squad:{name:"YOU DON'T KNOW ME!!!", quote:"This is a quote"}, userTeam:{name:"Amplifire", primary_color__c:"#EF6363"}});
+        component.set("v.contextInfo", {id:"00273731279", name:"Gladius Maximus", squad:{name:"YOU DON'T KNOW ME!!!", quote:"This is a quote"}, team:{name:"Amplifire", primary_color__c:"#EF6363"}});
         component.set("v.leadTeam", {name:"Amplife", primary_color__c:"#EF6363"});
         component.set("v.teamColors", "")
     }
