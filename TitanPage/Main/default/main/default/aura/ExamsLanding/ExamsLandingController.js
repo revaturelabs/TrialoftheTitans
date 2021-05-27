11fcb722dcdb8b404610aefb8a31368da7651193
abruce3411 <abruce3411@gmail.com>
2021-05-27 17:59:47 -0500

    updates 5/27 pm

--- TitanPage/Main/default/main/default/aura/ExamsLanding/ExamsLandingController.js
+++ TitanPage/Main/default/main/default/aura/ExamsLanding/ExamsLandingController.js
@@ -1,5 +1,13 @@
 ({
     cRenderExams : function(component, event, helper) {
 		helper.hRenderExams(component, event);
-    }
+    },
+    
+      doInit : function(component, event, helper) {
+        helper.hSetExams(component, event);
+    },
+    
+      cSetExams : function(component, event, helper) {
+        helper.hSetExams(component, event);
+    },
 })
--- TitanPage/Main/default/main/default/aura/ExamsLanding/ExamsLandingHelper.js
+++ TitanPage/Main/default/main/default/aura/ExamsLanding/ExamsLandingHelper.js
@@ -6,4 +6,19 @@
         });
         renderExams.fire();
     },
+    
+      hSetExams : function(component, event, helper) {
+        let activeTitan = component.get("v.activeTitan.Id");
+          console.log("Active Titan Id: " + activeTitan);
+        let examsList = component.get("c.getExams");
+        examsList.setParams({titanId : activeTitan});
+        examsList.setCallback(this, function(response){
+            if(response.getState() == "SUCCESS"){
+                component.set("v.examsList", response.getReturnValue());
+                console.log("Returned Exams: ");
+                console.log(response.getReturnValue());
+            }
+        })
+        $A.enqueueAction(examsList);
+    },
 })
