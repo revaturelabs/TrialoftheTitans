372b65d46c9c72d5d31634aaeb3efcad78c75587
The Farrier <nkfarrey@gmail.com>
2021-05-27 16:58:40 -0500

    drilled context info down to progress chart

--- HeroHubPackage/main/default/aura/HeroJourney/HeroJourneyController.js
+++ HeroHubPackage/main/default/aura/HeroJourney/HeroJourneyController.js
@@ -1,6 +1,9 @@
 ({
     doInit : function(component, event, helper) {
-		helper.HandleInit( component );
+		// helper.HandleInit( component );
+    var context = component.get("v.contextInfo")
+    console.log("Hero Journey checking in...")
+    console.log(context)
     },
     
     TitanClicked : function(component, event, helper){
