97db12d9faab69645fa3edc7db954047a6bc7ed4
The Farrier <nkfarrey@gmail.com>
2021-05-27 17:01:20 -0500

    Progress chart now recieves contextInfo

--- HeroHubPackage/main/default/aura/PageContainer/PageContainerController.js
+++ HeroHubPackage/main/default/aura/PageContainer/PageContainerController.js
@@ -2,11 +2,9 @@
     doInit: function( component, event, helper ) {
         const getContextInfo = component.get( "c.getUserInfo" );
         getContextInfo.setCallback( this, function( response ) {
-            console.log(response.getState());
             if( response.getState() === "SUCCESS" ){
                 const contextinfo = response.getReturnValue();
-                console.log( contextinfo );
-                component.set( "v.contextInfo", response.getReturnValue );
+                component.set( "v.contextInfo", contextinfo )
             }
             else {
                 //User isn't signed in return to login
@@ -18,15 +16,12 @@
         getTeamScores.setCallback( this, function( response ){
             console.log("TeamScores Response: " + response.getState());
             if( response.getState() === "SUCCESS"){
-                let teamScores = response.getReturnValue();
-                console.log(teamScores);
+                let teamScores = response.getReturnValue();   
                 Object.keys(teamScores).forEach(category =>{
                     teamScores[category] = Object.entries(teamScores[category]).sort((a,b) => {
                     return a[1] < b[1]? 1 : -1;
                 });
-                console.log(category, teamScores);
                 });
-            console.log(teamScores);
            }
         });
 
