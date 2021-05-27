5926c3762add53b457998c1e644b75fb1672ece8
David Serrano <davidserrano@Davids-MacBook-Pro-2.local>
2021-05-26 22:32:46 -0700

    fixed correct amount display, fixed completeed paths CSS, and fixed dynamic tab loading

--- HeroHubPackage/main/default/aura/GamePath/GamePathHelper.js
+++ HeroHubPackage/main/default/aura/GamePath/GamePathHelper.js
@@ -1,10 +1,13 @@
 ({
 	handleInit : function( component, event ) {
-        let exams = component.get("v.contextInfo.userExams");
+ 
+	},
+
+    handleGameChange : function( component, event){
         let examMap = new Map();
 
         // creating mock data
-        examMap.set( "Data Model", [ { name: "ERD", isPassed: true, highScore: 88.8 }, 
+        examMap.set( "Data Model", [{ name: "ERD", isPassed: true, highScore: 88.8 }, 
                                     { name: "Communism", isPassed: true, highScore: 78.8 },
                                     { name: "Shade", isPassed: true, highScore: 99.8 },
                                     { name: "Humble", isPassed: false, highScore: 42.0 },
@@ -15,76 +18,42 @@
                                     { name: "someName", isPassed: true, highScore: 81.5 } 
                                 ]);
 
-        examMap.set( "Triggers" , [ { name: "someName", isPassed: false, highScore: 42.0 },
-                                    { name: "someName", isPassed: false, highScore: 42.0 },
-                                    { name: "someName", isPassed: false, highScore: 42.0 },
-                                    { name: "someName", isPassed: false, highScore: 42.0 },
-                                    { name: "someName", isPassed: false, highScore: 42.0 }, 
-                                    { name: "someName", isPassed: false, highScore: 42.0 },
+        examMap.set( "Triggers" , [ { name: "someName1", isPassed: true, highScore: 99.0 },
+                                    { name: "someName2", isPassed: true, highScore: 87.0 },
+                                    { name: "someName3", isPassed: true, highScore: 73.0 },
+                                    { name: "someName4", isPassed: false, highScore: 42.0 },
+                                    { name: "someName5", isPassed: false, highScore: 42.0 }, 
+                                    { name: "someName6", isPassed: false, highScore: 42.0 },
                                     ]);
          
-        console.log("updated active:" + component.get("v.active"));
-        exams = examMap.get(component.get("v.active"));             
-        let passedExams = [];
-        let failedExams = [];
-        
-        for(let i in exams){
-            if(exams[i].isPassed){
-                passedExams.push(exams[i]);
-               
-            } else {
-                failedExams.push(exams[i]);
+        console.log("updated active: " + component.get("v.active"));
+        if(component.get("v.active") != "Overview"){
+            
+        let exams = examMap.get(component.get("v.active"));             
+        let iterableExams = [];
+
+        let firstExam = exams[0];
+        let finalExam = exams[exams.length - 1];
+
+        for(let exam in exams){
+            if(exam == 0 || exam == (exams.length - 1)){
+
+            }else{
+                iterableExams.push(exams[exam]);
             }
         }
 
-        let firstExam;
-        let finalExam;
-        // account for no failed exams (all passed)
-        // account for all "failed" exams (none taken yet)
-        if( exams != null && passedExams != null && passedExams.length == exams.length ){
-            firstExam = passedExams[0];
-            finalExam = passedExams[ passedExams.length - 1 ];
-            passedExams.pop();
-            passedExams.shift();
-
-            component.set("v.firstExam", firstExam);
-            component.set("v.finalExam", finalExam);
-            component.set("v.passedExams", passedExams);
-            component.set("v.exams", exams);
-        } else if( exams != null && failedExams != null && failedExams.length == exams.length ){
-            firstExam = failedExams[0];
-            finalExam = failedExams[ failedExams.length - 1 ];
-            failedExams.pop();
-            failedExams.shift();
-
-            component.set("v.firstExam", firstExam);
-            component.set("v.finalExam", finalExam);
-            component.set("v.failedExams", passedExams);
-            component.set("v.exams", exams);
-        } else {
-            firstExam = passedExams[0];
-            finalExam = failedExams[failedExams.length-1];
-            passedExams.shift();
-            failedExams.pop();
-
-            component.set("v.firstExam", firstExam);
-            component.set("v.finalExam", finalExam);
-            component.set("v.passedExams", passedExams);
-            component.set("v.failedExams", passedExams);
-            component.set("v.exams", exams);
-        }
+        
+        component.set("v.exams", exams);
+        component.set("v.firstExam", firstExam);
+        component.set("v.finalExam", finalExam);
+        component.set("v.iterableExams", iterableExams);
 
         console.log("exam: " + JSON.stringify(exams));
         console.log("first exam: " + JSON.stringify(firstExam));
         console.log("final exam: " + JSON.stringify(finalExam));
-        console.log("passed exams: " + JSON.stringify(passedExams));
-        console.log("failed exams: " + JSON.stringify(failedExams));
+        console.log("iterable exam: " + JSON.stringify(iterableExams));
 
-	},
-
-    handleGameChange : function( component, event){
-        let method = component.get("c.GameInit");
-        method.setParams({"component" : component, "event" : event});
-        $A.enqueueAction(method);
+        }
     }
 })
