875d879e03265d6f119ca81d429d5fe6214bf7b3
Josh Miccolo <65175521+Jmiccolo@users.noreply.github.com>
2021-05-28 14:26:51 -0400

    Fixing unrender

--- HeroHubPackage/main/default/aura/TitanSlayer/TitanSlayerController.js
+++ HeroHubPackage/main/default/aura/TitanSlayer/TitanSlayerController.js
@@ -1,12 +1,20 @@
 ({
 	doInit : function(component, event, helper) {
-		const flipInterval = setInterval(function(){
+		const flipInterval = window.setInterval($A.getCallback(function(){
 			const titanSlayer = document.querySelector('.titanSlayer');
 			titanSlayer.classList.toggle('hide');
-			setTimeout(function(){
+			const titanTimeout = window.setTimeout($A.getCallback(function(){
 				titanSlayer.classList.toggle('switch');
 				titanSlayer.classList.toggle('hide');
-			}, 1000)
-		}, 10000);
+			}), 1000);
+			component.set('v.setTimeoutId', titanTimeout);
+		}), 10000);
+		
+		component.set('v.setIntervalId', flipInterval);
+	},
+
+	handleClear : function(component, event, helper) {
+		 window.clearInterval(component.get("v.setIntervalId"));
+		 window.clearTimeout(component.get('v.setTimeoutId'));
 	}
 })
