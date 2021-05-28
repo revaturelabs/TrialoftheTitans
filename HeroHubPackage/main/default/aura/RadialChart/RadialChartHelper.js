f561850b07e38dee70f42eed5e7a9ba0bcba2bc7
andreology27 <37252145+andreology@users.noreply.github.com>
2021-05-28 12:24:40 -0700

    creating pallette for chart team colors

--- HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
+++ HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
@@ -2,11 +2,11 @@
     drawChart : function(component, event) {
         let data = component.get('v.contextInfo');
         console.log("made it to helper ");
-       
+        console.log(data);
         const width  = 1100;
         const height = 1400;
         const margin = 100;
-     
+        
         let cmp = component.find('mydthree').getElement();
         let titans = {
             "Apex": [
@@ -79,6 +79,18 @@
                     }]
         };
         console.log("loaded data " + titans);
-        
+        /* ------------------------------ CODE STARTS ------------------------------------------------------------*/
+        let currKey = 0;
+        let sourceData = {};
+        let trackExamAverage = [];
+        let trackLabels = [];
+        let currHighScores = [];
+        let teamColors = new Map();
+        teamColors.set('Amplifire', '#C24747');
+        teamColors.set('Synergy', '#F4CF38');
+        teamColors.set('Alchemy', '#84C247');
+        teamColors.set('Vanquish', '#475BC2');
+        teamColors.set('Avg', 'black');
+
     }
 })
