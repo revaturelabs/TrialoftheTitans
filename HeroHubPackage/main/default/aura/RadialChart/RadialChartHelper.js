5e5e22eda8a657a3747f3880a5a2abeabd684eb7
andreology27 <37252145+andreology@users.noreply.github.com>
2021-05-28 12:22:41 -0700

    creating dummy data test chart

--- HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
+++ HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
@@ -1,7 +1,84 @@
 ({
     drawChart : function(component, event) {
         let data = component.get('v.contextInfo');
-        console.log("checking data ");
-        console.log(data);
+        console.log("made it to helper ");
+       
+        const width  = 1100;
+        const height = 1400;
+        const margin = 100;
+     
+        let cmp = component.find('mydthree').getElement();
+        let titans = {
+            "Apex": [
+                {
+                    "currentResults": [],
+                    "highScore": .49,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                },
+                {
+                    "currentResults": [],
+                    "highScore": .89,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                }],
+            "Data Modeling": [
+                {
+                    "currentResults": [],
+                    "highScore": .29,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                },
+                {
+                    "currentResults": [],
+                    "highScore": .19,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                }],
+            "Process Automation": [
+                {
+                    "currentResults": [],
+                    "highScore": .69,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                },
+                {
+                    "currentResults": [],
+                    "highScore": .29,
+                    "isPassed": false,
+                    "name": "ello",
+                    "nextExam": "asdfasdf",
+                    "pastResult": []
+                }],
+                "Security": [
+                    {
+                        "currentResults": [],
+                        "highScore": .19,
+                        "isPassed": false,
+                        "name": "ello",
+                        "nextExam": "asdfasdf",
+                        "pastResult": []
+                    },
+                    {
+                        "currentResults": [],
+                        "highScore": .69,
+                        "isPassed": false,
+                        "name": "ello",
+                        "nextExam": "asdfasdf",
+                        "pastResult": []
+                    }]
+        };
+        console.log("loaded data " + titans);
+        
     }
 })
