c0bb0b3cc41698f29ccf9448425f03070e8be929
andreology27 <37252145+andreology@users.noreply.github.com>
2021-05-28 12:25:41 -0700

    creating radial chart using d3.js

--- HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
+++ HeroHubPackage/main/default/aura/RadialChart/RadialChartHelper.js
@@ -122,6 +122,58 @@
             "name" : "highscores",
             "averages": currHighScores
        };      
+       const colorScale = d3.scaleSequential(d3.interpolateSpectral)
+                         .domain([0,sourceData.length]);
+        const scaleRadius = d3.scaleLinear()
+                        .domain([0, 1])
+                        .range([0, width/2 - margin]);
+        const spiral = d3.areaRadial()
+                        .angle((d,i) =>  i/4 * Math.PI * 2)
+                        .outerRadius(d => scaleRadius(d));
+        const axis = d3.axisBottom(scaleRadius)
+                         .ticks(6)
+                        .tickSize(0);
+
+        const svg = d3.select("body")
+                    .append("svg")
+                    .attr("height",height)
+                    .attr("width",width);
+
+        const g = svg.append("g")
+            .attr("transform","translate(600, 800)");
+        let inputData = [];
+        inputData.push(sourceData);
+        inputData.push(highScoreData);
+        let avgFlag = false;
+
+        const averageRadial =  g.selectAll("path").data(inputData).join("path")
+            .attr("d", (d, i) => spiral(d.averages))
+             .style("fill-opacity", .1)
+             .style("stroke", (d,i) => i == 0 ? teamColors.get(userTeam) : teamColors.get('Avg'))
+             .style("stroke-width", (d, i) => i==0 ? 8 : 2)
+             .style("fill", (d,i) => teamColors.get(userTeam));
+
+        const max = d3.max(inputData[0].averages, d => d);
+
+        g.selectAll("g.axis")
+                 .data(inputData[0].averages).join("g")
+                 .attr("class", "axis")
+                  .classed("blank",(d,i) => i != 0)
+                  .call(axis)
+                .transition().delay(3000).duration(2000)
+                  .attr("transform", (d,i) => `rotate(${(i * 360/(4))})`);
+
+        g.selectAll("g.axis").data(inputData[0].averages)
+                  .append("text")
+                 .style("font-size", "50px").style("fill", "black").style("text-anchor", "middle")
+                 .text((d,i) => trackLabels[i])
+                 .attr("transform", d => `translate(0, ${scaleRadius(1) + 40})`);
+
+
+             
+      /*----------------------------------------CODE ENDS --------------------------------------------------------------*/
+                                       
+        
 
     }
 })
