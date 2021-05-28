c38fe266c5dd0df45ea5eaa150aed57c8b1f4e7b
andreology27 <37252145+andreology@users.noreply.github.com>
2021-05-28 11:13:48 -0700

    creating aura if to check data is available before drawing graphs

--- HeroHubPackage/main/default/aura/TitanProgressRadialChart/TitanProgressRadialChartController.js
+++ HeroHubPackage/main/default/aura/TitanProgressRadialChart/TitanProgressRadialChartController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
+    drawRadialChart : function(component, event, helper) {
+        helper.drawChart(component, event);
     }
 })
--- HeroHubPackage/main/default/aura/TitanProgressRadialChart/TitanProgressRadialChartHelper.js
+++ HeroHubPackage/main/default/aura/TitanProgressRadialChart/TitanProgressRadialChartHelper.js
@@ -1,5 +1,5 @@
 ({
-    helperMethod : function() {
+    drawChart : function() {
 
     }
 })
