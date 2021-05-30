562ffbd94bd5b5b72973e8eddec550b1bdfcea8c
Josh Miccolo <65175521+Jmiccolo@users.noreply.github.com>
2021-05-30 02:34:34 -0400

    Cleaning up Set Timeouts and Intervals(Still not working)

--- HeroHubPackage/main/default/aura/RadialChart/RadialChartController.js
+++ HeroHubPackage/main/default/aura/RadialChart/RadialChartController.js
@@ -1,9 +1,7 @@
 ({
     GenerateRadialChart: function(component, event, helper) {
-        console.log("From Radial Chart Controller");
 		// Only render chart if we have both d3.js and data loaded
 		if(component.get( "v.scriptsLoaded" ) && component.get( "v.contextInfo" ) ){
-        console.log("Both loaded");
         const titans = component.get( "v.contextInfo" ).userExams;
 		helper.drawChart( component, event, titans );	
 		}
