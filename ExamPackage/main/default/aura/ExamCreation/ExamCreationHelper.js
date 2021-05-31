7af4ebf8180e20e47f0a7ce9ee43b36cfe5473cf
DanTFree <47703077+DanTFree@users.noreply.github.com>
2021-05-31 12:33:44 -0700

    Add changes to Permission Set of trainer and Sysadmin Commented on Exam Creation Handler and Component.

--- ExamPackage/main/default/aura/ExamCreation/ExamCreationHelper.js
+++ ExamPackage/main/default/aura/ExamCreation/ExamCreationHelper.js
@@ -1,7 +1,7 @@
 ({
     // get questions pools from server
     // currently getting all of them
-    // need some search or filter functionality.
+    // unless searchbar is triggered
     getQuestionPools : function(component) {
         let action = component.get("c.getQuestionPool");
         let searchKeyword = component.get("v.searchKeyword");
@@ -49,7 +49,7 @@
         });
        	$A.enqueueAction(action);
     },
-    // just send the information to the server for insert
+    // Send the information to the server for insert
     // poolAndNumber is a map
     createExamAssignment : function(component, poolAndNumber, examId){
         let action = component.get("c.createExamAssignment");
