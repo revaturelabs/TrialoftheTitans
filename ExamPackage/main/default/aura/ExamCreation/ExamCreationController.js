c1f324d5c17c33c712d7d716dcade1cff3e47ee0
DanTFree <47703077+DanTFree@users.noreply.github.com>
2021-06-01 08:29:46 -0700

    Fix method names, remove unnecessary variables. Add field to related list of Exams

--- ExamPackage/main/default/aura/ExamCreation/ExamCreationController.js
+++ ExamPackage/main/default/aura/ExamCreation/ExamCreationController.js
@@ -1,6 +1,6 @@
 ({
     // get questions pools and set the columns of the two datatable
-    init : function(component, event, helper){
+    Init : function(component, event, helper){
         
         // control each column of data table
         // left table
@@ -28,29 +28,29 @@
                           
                       ]);
         // get question pools 
-        helper.getQuestionPools(component);
+        helper.GetQuestionPools(component);
     },
     // show the questions in a pool, when clicking questions button in any row.
     // currently querying everytime a questions button is clicked.
     // should be fine, since they unlikely to review all the questions in every pool.
-    showQuestionInPool : function(component, event, helper){
+    ShowQuestionInPool : function(component, event, helper){
         let payload = event.getParam('row').poolId;
-        helper.showQuestions(component, payload);
+        helper.ShowQuestions(component, payload);
     },
     // save the exam Id on creation of exam.
     // use to insert question pool.
-    handleSuccess : function(component, event, helper){
+    HandleSuccess : function(component, event, helper){
         let examId = event.getParam("id");
         component.set("v.examId", examId);
         component.set("v.examCreated", true);
     },
     // send all modified question pool to be add to question assignment.
-    handleCreation : function(component, event, helper){
+    HandleCreation : function(component, event, helper){
         component.set("v.examCreated", false);
-        helper.createExamAssignment(component, component.get("v.draftValuesMap"), component.get("v.examId"));
+        helper.CreateExamAssignment(component, component.get("v.draftValuesMap"), component.get("v.examId"));
     },
     // update map on inline edit of question pool
-    updateQuestionQuantity : function(component, event, helper){
+    UpdateQuestionQuantity : function(component, event, helper){
         let draftVal = event.getParam('draftValues');
         let draftMap = component.get("v.draftValuesMap");
         let questionPool = component.get("v.questionPool");
@@ -60,7 +60,7 @@
         component.set("v.draftValuesMap", draftMap);
     },
     // go to next page
-    handlePrev : function(component, event, helper){
+    HandlePrev : function(component, event, helper){
         let myMap = component.get("v.questionPoolMap");
         let currentPage = component.get("v.currentPage") - 1;
         component.set("v.questionPool", myMap[currentPage]);
@@ -68,7 +68,7 @@
         component.set("v.isLastPage", false);
     },
     // go to previous page
-    handleNext : function(component, event ,helper){
+    HandleNext : function(component, event ,helper){
         let myMap = component.get("v.questionPoolMap");
         let currentPage = component.get("v.currentPage")+1;
         component.set("v.questionPool", myMap[currentPage]);
