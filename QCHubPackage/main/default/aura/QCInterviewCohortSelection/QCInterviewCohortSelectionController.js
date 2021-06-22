d56ec512502df3f2466c735d2fbc0eb0f240f825
Natalie Clark <moonshine.machiavelli@gmail.com>
2021-06-22 17:33:57 -0400

    work done on Cohort selection component and added apex controller class

--- QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionController.js
+++ QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionController.js
@@ -1,5 +1,37 @@
 ({
-    myAction : function(component, event, helper) {
+    // initialize (duh): creates columns for datatable
+    init : function(component, event) {
+        component.set("v.columns", 
+                    [
+                        {label:'Hero', fieldName=""},
+                        {label:'Score', fieldName=""},
+                        {label:'Status', fieldName=""},
+                        {label:'Interview', type:'button', 
+                            typeAttributes: {
+                                    name: 'start',
+                                    label: 'Start',
+                                    iconPosition: 'left',
+                                    iconName: 'utility:warning',
+                                    variant: 'warning' 
+                            }
+                        }
+                    ]
+        )
+    },
 
-    }
+    // row selection handles button to start interview
+    handleRowSelection : function(component, event, helper) {
+        helper.handleRowSelection(component, event)
+    },
+
 })
+
+// {type : 'button', label: 'Interview',
+// typeAttributes: {
+//         name: 'completed',
+//         label: 'Complete',
+//         iconPosition: 'right',
+//         iconName: 'utility:success',
+//         variant: 'Success' 
+// }
+// }
--- QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionHelper.js
+++ QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionHelper.js
@@ -1,5 +1,5 @@
 ({
-    helperMethod : function() {
+    handleRowSelection : function(component, event) {
 
     }
 })
