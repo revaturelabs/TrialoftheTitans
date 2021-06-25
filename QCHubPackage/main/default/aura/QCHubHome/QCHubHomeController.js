50a93e6beb2d54cbadc14421692fc6004c127d81
Alexander Dzhitenov <alexander.dzhitenov@revature.net>
2021-06-25 12:41:37 -0400

    Fixed cohort selection not working again, removed files I was not working on from my local branch to prevent conflicts

--- QCHubPackage/main/default/aura/QCHubHome/QCHubHomeController.js
+++ QCHubPackage/main/default/aura/QCHubHome/QCHubHomeController.js
@@ -6,10 +6,8 @@
     },
 
     UpdateCohort : function(component, event, helper){
-        //let selectedCohort = event.getParam("SelectedCohort");
-        //console.log("QCHUBHOME HANDLER: " + selectedCohort);
-        //console.log("cObj in String Format===>"+JSON.stringify(selectedCohort) );
-        helper.LoadCohortData(component, event.getParam("SelectedCohort"))
+        console.log("QCHUBHOME HANDLER");
+        helper.LoadCohortData(component, event.getParam("SelectedCohort"));
         component.set("v.NoCohortSelected", false);
         
     },
