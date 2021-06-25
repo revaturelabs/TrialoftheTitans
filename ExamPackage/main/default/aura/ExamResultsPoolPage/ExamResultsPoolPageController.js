6773df163be9b979553a444b0688bff188396336
Miguel McCrary <migmccrary@gmail.com>
2021-06-24 23:03:45 -0500

    aura component calculates correct answer percentage per pool.

--- ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageController.js
+++ ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageController.js
@@ -1,56 +1,52 @@
 ({
     doInit : function(component, event, helper) {
-        let getExamQuestions = component.get("c.getExamResultPools");
-        let numberCorrect = 3;
-        let hero;
-        getExamQuestions.setCallback( this, function( response ) {
+        let getExamPoolQuestions = component.get("c.getExamResultPools");
+
+        getExamPoolQuestions.setCallback( this, function( response ) {
             if( response.getState() === "SUCCESS" ){
-                const ExamQuestionResults = response.getReturnValue();
-                for(let i in ExamQuestionResults){
-                    console.log('Before Divide ' + ExamQuestionResults[i].Number_to_pull__c)
-                    ExamQuestionResults[i].Number_to_pull__c = numberCorrect/ExamQuestionResults[i].Number_to_pull__c
-                    console.log(ExamQuestionResults[i].Question_Pool__r.Name + " " + ExamQuestionResults[i].Number_to_pull__c)
-                    
-                }
-                console.log("Getting Exam Question Info", ExamQuestionResults);
-                component.set( "v.questionPool", ExamQuestionResults );
+                var pool = [];
+                const  AllExamPoolQuestions = response.getReturnValue();
+     
+                console.log("Getting Exam Question Info",  AllExamPoolQuestions);
+                component.set( "v.questionPool",  AllExamPoolQuestions );
+                component.set( "v.correctpoollist",  AllExamPoolQuestions );
             }
             else {
                 console.log('No Pools in this Exam')
             }
         });
-        $A.enqueueAction(getExamQuestions);
+        $A.enqueueAction(getExamPoolQuestions);
 
     },
 
     poolEvent : function(component, event, helper){
         let getCorrectAnswerPool = component.get("c.getExamResultAnswers");
-
+        //create an object to count frequency through key/value pairs
+        
         getCorrectAnswerPool.setCallback( this, function( response ) {
             if( response.getState() === "SUCCESS" ){
+                let resetPool = component.get("v.correctpoollist")
                 const CorrectAnswerPool = response.getReturnValue();
-                console.log("Correct Answers from each pool", CorrectAnswerPool);
-                component.set( "v.poolmap", CorrectAnswerPool);
+                component.set( "v.questionPool",  resetPool);
+
+               helper.correctExamPoolQuestionsMapped(component, CorrectAnswerPool);
+
             }
             else {
                 console.log('No correct answers in a pool')
             }
         });
         $A.enqueueAction(getCorrectAnswerPool);
-
+        helper.ExamPoolFormToggle(component, event, helper);
     },
     
-    navigater : function(component, event, helper){
+   /*navigater : function(component, event, helper){
 
         console.log('hit');
         const page = event.getParam('page');
         console.log(page);
         component.set("v.navigate", page);
 
-    },
+    },*/
 
-    ExamPoolFormToggle : function(component, event, helper) {
-        component.set("v.revealPoolInfo", !component.get("v.revealPoolInfo"));
-    },
-    
 })
--- ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageHelper.js
+++ ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageHelper.js
@@ -1,5 +1,73 @@
 ({
-    helperMethod : function() {
+    /*allExamPoolQuestionsMapped : function(component, finalflash) {
+    const cap = {};
+    finalflash.forEach(item => {
+        if(cap[item]){
+        cap[item]++;
+        }else{
+        cap[item] = 1;
+        }
+        });
+        component.set("v.questionPool", cap);
+        console.log("All Pool Answers Below:");
+        console.log(component.get("v.questionPool"));
+    },
+    */
+    ExamPoolFormToggle : function(component, event, helper) {
+        component.set("v.revealPoolInfo", !component.get("v.revealPoolInfo"));
+    },
 
-    }
+    correctExamPoolQuestionsMapped : function(component, CorrectAnswerPool) {
+        const cap = {};
+        const cap2 ={};
+        const finmap ={};
+        const obj ={};
+        const finalflash =  JSON.parse(JSON.stringify(component.get("v.questionPool")));
+        CorrectAnswerPool.forEach(item => {
+            if(cap[item]){
+            cap[item]++;
+            }else{
+            cap[item] = 1;
+            }
+            });
+
+            finalflash.forEach(item => {
+                if(cap2[item]){
+                cap2[item]++;
+                }else{
+                cap2[item] = 1;
+                }
+                });
+
+                
+
+                
+                
+                let key;
+                let initValue;
+                let fixedValue;
+                for (let i = 0; i < Object.keys(cap).length; i++){ 
+                    for (let j = 0; j < Object.keys(cap2).length; j++){
+                        console.log(Object.keys(cap)[i] + " " + Object.keys(cap2)[j])
+                        if(Object.keys(cap)[i] == Object.keys(cap2)[j]){
+                            console.log("Delivery");
+                            key = Object.keys(cap)[i];
+                            initValue = (cap[Object.keys(cap)[i]] / cap2[Object.keys(cap2)[j]]) * 100;
+                            fixedValue = initValue.toFixed(2); 
+                            finmap[key] = fixedValue; 
+                        }
+                    }
+                }
+                var output = Object.entries(finmap).map(([key, value]) => ({key,value}));
+
+            component.set("v.questionPool", output);
+            console.log("Attribute Done?");
+            console.log(component.get("v.questionPool"));
+            console.log("Correct Answers: ");
+            console.log(cap);
+            console.log("All Answers: ");
+            console.log(cap2);  
+        },
+
+        
 })
