8cc2faaf8d30260e1093f1c0d9d7af930bf02d27
Wootae Yang <56479033+wyang19a@users.noreply.github.com>
2021-06-26 17:54:24 -0400

    Pulls in data dynamically
    Co-authored-by: Miguel <migmccrary@gmail.com>

--- ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageController.js
+++ ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageController.js
@@ -2,7 +2,10 @@
     //retrieve all questions from exam results that are within a question pool and set to an attribute
     DoInit : function(component, event, helper) {
         let getExamPoolQuestions = component.get("c.GetAllExamResultPoolQuestions");
-        //set.Param with examResultId
+        console.log('HIIIIII' + component.get('v.examResultId'))
+        getExamPoolQuestions.setParams({
+            'examResultId': component.get('v.examResultId')
+        })
         getExamPoolQuestions.setCallback( this, function( response ) {
             if( response.getState() === "SUCCESS" ){
 
@@ -21,7 +24,10 @@
     //retrieve all correctly answered questions from exam results that are within a question pool
     RetrieveCorrectExamResultPools : function(component, event, helper){
         let getCorrectAnswerPool = component.get("c.GetCorrectExamResultPoolQuestions");
-        
+        console.log('HIIIIII' + component.get('v.examResultId'))
+        getCorrectAnswerPool.setParams({
+            'examResultId': component.get('v.examResultId')
+        })
         getCorrectAnswerPool.setCallback( this, function( response ) {
             if( response.getState() === "SUCCESS" ){
                 let resetPool = component.get("v.QuestionPoolReset")
