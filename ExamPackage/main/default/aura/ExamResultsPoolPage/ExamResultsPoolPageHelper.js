fd02cf764b522373c611a8171995e8a407e406fe
wyang19a <wyang19a@gmail.com>
2021-06-29 09:22:31 -0400

    Adds bar chart

--- ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageHelper.js
+++ ExamPackage/main/default/aura/ExamResultsPoolPage/ExamResultsPoolPageHelper.js
@@ -6,7 +6,7 @@
 
     //creates an object for all exam result questions in a pool 
     //and all correct exam result questions in a pool, compares the pool
-    CalculatePoolPercentage : function(component, correctExamResultPoolQuestions) {
+    CalculatePoolPercentage : function(component, event, correctExamResultPoolQuestions) {
         let correctPoolQuestionsObj = {};
         let allPoolQuestionsObj ={};
         let resultsObject ={};
@@ -52,14 +52,101 @@
                     }
                 }
                 let mapForAttribute = Object.entries(resultsObject).map(([key, value]) => ({key,value}));
-
-            component.set("v.ExamResultPoolQuestions", mapForAttribute);
-            console.log("Attribute Finalized: ");
-            console.log(component.get("v.ExamResultPoolQuestions"));
-            console.log("Correct Answers: ");
-            console.log(correctPoolQuestionsObj);
-            console.log("All Answers: ");
-            console.log(allPoolQuestionsObj);  
+                component.set("v.ExamResultPoolQuestions", mapForAttribute);
+                this.drawD3(component, event, mapForAttribute)
+            // console.log("Attribute Finalized: ");
+            // console.log(component.get("v.ExamResultPoolQuestions"));
+            // console.log("Correct Answers: ");
+            // console.log(correctPoolQuestionsObj);
+            // console.log("All Answers: ");
+            // console.log(allPoolQuestionsObj);  
         },
-   
-})
+        drawD3: function(cmp, event, mapForAttribute) {
+
+            let obj = {};
+            mapForAttribute.forEach(pair => {
+                obj[pair.key] = pair.value
+            })
+            let sortable = [];
+            for (let key in obj) {
+                sortable.push([key, obj[key]])
+            }
+            sortable.sort(function(a, b) {
+                return b[1] - a[1];
+            })
+            let objSorted = {}
+            sortable.forEach(pair => {
+                objSorted[pair[0]] = pair[1]
+            })
+            
+            for (let i = 0; i < Object.keys(objSorted).length; i++) {
+                // console.log(myJson[Object.keys(myJson)[i]])
+                // myHtml += `<div data-score="${myJson[Object.keys(myJson)[i]]}"></div>`
+                $A.createComponent(
+                    "aura:html", {
+                        "tag": 'div',
+                        "body": `${Object.keys(objSorted)[i]}...${objSorted[Object.keys(objSorted)[i]]}%`,
+                        "HTMLAttributes": {
+                            "style": `--i:${objSorted[Object.keys(objSorted)[i]]}%; --y:${objSorted[Object.keys(objSorted)[i]] * 0.9}%`,
+                            // "onclick": cmp.getReference("c.onExamClick"),
+                            "class": 'bar-chart',
+                            // "data-score": `${myJson[Object.keys(myJson)[i]]}%`
+                        }
+                    },
+                    function(newButton, status, errorMessage){
+                        //Add the new button to the body array
+                        if (status === "SUCCESS") {
+                            var body = cmp.get("v.body");
+                            body.push(newButton);
+                            cmp.set("v.body", body);
+                        }
+                        else if (status === "INCOMPLETE") {
+                            console.log("No response from server or client is offline.")
+                            // Show offline error
+                        }
+                        else if (status === "ERROR") {
+                            console.log("Error: " + errorMessage);
+                            // Show error message
+                        }
+                    }
+                );
+
+            }
+            
+            
+        }
+        
+    })
+    
+    
+                // let width = 960;
+                // let height = 500;
+    
+                // let svg = d3.select(".test-div")
+                //             .append("svg")
+                //             .attr("width", width)
+                //             .attr("height", height)
+                //             .append('g')
+    
+                // let scale = d3.scaleLinear()
+                // .range([0, 300])
+                // .domain([0, 1000])
+    
+                // const xExtent = d3.extent(barChartData, d )
+                // let data = myJson;
+    
+                // let 
+                // console.log('looking here', JSON.stringify(mapForAttribute))
+                // console.log('myJson', `${JSON.stringify(myJson)}`)
+                // // console.log(myObj, myJson)
+    
+                // // d3.json(JSON.stringify(myJson), function(error, data) {
+                // //     if (error) {
+                // //         throw error
+                // //     }
+                // //     console.log(data)
+                // // })
+    
+                // d3.json(myJson).then(res => {
+                //     console.log('d3res', res)
+                // })
