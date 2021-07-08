4b31ac369ac4cd93793800200ed8d2f958983a09
Wourohire Laurent Sanou <sanouwlaurent@gmail.com>
2021-07-08 15:45:40 -0400

    added code to update selectedProjId attribute from assign action in controller

--- TrainerHubPackage/main/default/aura/projectHome/projectHomeController.js
+++ TrainerHubPackage/main/default/aura/projectHome/projectHomeController.js
@@ -14,38 +14,26 @@
     
     
     
-     edit: function(component, event, helper) {
-<<<<<<< HEAD
+    edit: function(component, event, helper) {
         console.log("edit")
-
-        component.set("v.selectedProjId", event.target.id)
-        component.set("v.currentPage", "dynamicRow");
-
-=======
-        console.log("edit");
-        console.log(event.target.id);
-        component.set("v.currentPage", "dynamicRow");
         component.set("v.selectedProjId", event.target.id);
->>>>>>> StevenMagnin
+        component.set("v.currentPage", "dynamicRow");
     },
     
     
     more: function(component, event, helper) {
         console.log("more")
-         component.set("v.currentPage", "morePage");
+        component.set("v.currentPage", "morePage");
     },
     
     assign: function(component, event, helper) {
-        console.log("assign")        
-         component.set("v.currentPage", "assignmentPage");
+        console.log("assign")   
+        component.set("v.selectedProjId", event.target.id);     
+        component.set("v.currentPage", "assignmentPage");
     },
     
     addNewProject: function(component, event, helper) {
         console.log("addNewProject")   
-<<<<<<< HEAD
-         component.set("v.currentPage", "AddNewProjectPage");
-=======
-         component.set("v.currentPage", "NewProjectCreation");
->>>>>>> StevenMagnin
+        component.set("v.currentPage", "AddNewProjectPage");
     },
 })
