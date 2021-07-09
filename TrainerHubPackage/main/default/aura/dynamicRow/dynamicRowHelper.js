0438abadc4cf00f80a9b8f235dfae401647f3b33
Nathan T <nathanvtellez@gmail.com>
2021-07-09 11:59:12 -0700

    styling

--- TrainerHubPackage/main/default/aura/dynamicRow/dynamicRowHelper.js
+++ TrainerHubPackage/main/default/aura/dynamicRow/dynamicRowHelper.js
@@ -14,30 +14,33 @@
     retrieveCurrentEquivalencies: function(component){
         let action = component.get("c.retrieveCurrentEquivalencies");
         action.setParams({projectId: component.get("v.projectId")});
-        action.setCallback(this,function(response){
+        action.setCallback(this, function(response){
             if(response.getState() === 'SUCCESS'){
-                console.log(response.getReturnValue());
                 component.set("v.currentEquivalencies", response.getReturnValue());
-            }else if(response.getState() === 'ERROR'){
-
             }
         });
         $A.enqueueAction(action);
     },
+
     // helper function to check if skill__c is not null/blank on save  
     validateRequired: function(component, event) {
         let isValid = true;
         let uniqueSkills = new Set();
         let allEquivRows = component.get("v.equivList");
-        console.log(allEquivRows);
+        let currentEquivs = component.get("v.currentEquivalencies");
+
+        for(let indexVar = 0; indexVar < currentEquivs.length; indexVar++){
+            uniqueSkills.add(currentEquivs[indexVar].Skill__c);
+        }
+
         for (let indexVar = 0; indexVar < allEquivRows.length; indexVar++) {
-           if (allEquivRows[indexVar].Skill__c == '') {
+            if (allEquivRows[indexVar].Skill__c == '') {
                 isValid = false;
-                alert('Skill Can\'t be Blank on Row Number ' + (indexVar + 1));
+                alert('Skill can\'t be blank on row ' + (indexVar + 1));
             }
             else if (uniqueSkills.has(allEquivRows[indexVar].Skill__c)){
                 isValid = false;
-                alert('Can\'t add the same skill more than once.');
+                alert('Can\'t add the same skill on row ' + (indexVar + 1));
             }
             else{
                 uniqueSkills.add(allEquivRows[indexVar].Skill__c);
