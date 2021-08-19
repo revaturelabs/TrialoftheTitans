b21f8804c9bb6f9baedebe7186932c70a338dbe6
Th3Pope <48426524+Th3Pope@users.noreply.github.com>
2021-08-19 11:47:43 -0700

    possibly made a pushable version

--- QCHubPackage/main/default/lwc/d3ChartTest/d3ChartTest.js
+++ QCHubPackage/main/default/lwc/d3ChartTest/d3ChartTest.js
@@ -3,7 +3,7 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
-import D3 from '@salesforce/resourceUrl/d37';
+import D3 from '@salesforce/resourceUrl/d3';
 import DATA from './data';
 
 export default class LibsD3 extends LightningElement {
