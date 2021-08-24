bc4f0469ef1fdb82ce27214a33fbd5fc43326b9a
Theodore Mitchell <softwaretheodore@gmail.com>
2021-08-24 17:02:30 -0500

    Aug 24 changes

--- MockInterviewPackage/main/default/aura/TheoTestHarnessApp/TheoTestHarnessApp.app
+++ MockInterviewPackage/main/default/aura/TheoTestHarnessApp/TheoTestHarnessApp.app
@@ -1,8 +1,19 @@
 <aura:application extends='force:slds'>
-    <c:QCInterview2 />
-    <!-- Test comment here? -->
-    <c:qcInterview2LWC />
+    Aura
+    <c:QCInterview2 CohortId='a0F1700000JFSuFEAX'/>
+    LWC
+    <c:qcInterview2LWC cohortId='a0F1700000JFSuFEAX'/>
+    Aura
+    <c:QCInterview2 CohortId='a0F1700000JFSuPEAX'/>
+    LWC
+    <c:qcInterview2LWC cohortId='a0F1700000JFSuPEAX'/>
+    Aura
+    <c:QCInterview2 CohortId=''/>
+    LWC
+    <c:qcInterview2LWC cohortId=''/>
     <c:HeroGroupchart />
     <c:HeroList/>
-    <c:NewProjectCreation/>
+    <!--<c:NewProjectCreation/>-->
+    <c:TrainerHubAndCohortsPage/>
+    <c:projectHome />
 </aura:application>
--- TrainerHubPackage/main/default/lwc/qcInterview2LWC/qcInterview2LWC.js
+++ TrainerHubPackage/main/default/lwc/qcInterview2LWC/qcInterview2LWC.js
@@ -1,3 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import InterviewList from '@salesforce/apex/QCInterviewController.InterviewList'
 
-export default class QcInterview2LWC extends LightningElement {}
+const columns = [
+    { label: 'QC Interview', fieldName: 'Name', type: 'text'},
+    { label: 'Hero', fieldName: 'hero', type: 'text'},
+    { label: 'QC Week', fieldName: 'Week__c', type: 'text'},
+    { label: 'QC Score', fieldName: 'QC_Score__c', type: 'text'},
+];
+
+export default class QcInterview2LWC extends LightningElement {
+
+    columns = columns;
+    data = [];
+    
+    @api
+    cohortId;
+
+
+    connectedCallback() {
+        // InterviewList({cohortId: '$cohortId'})
+
+        InterviewList({cohortId: ''})
+        .then(result => {
+            if (result) {
+                let newData = [];
+
+                //for loop to set key for the coloumn with data
+                for( let i=0; i< result.length; i++ ){
+                    //if data retrieved is null place value stating it is null
+                    //if not null place data in the key for the column
+                    if(result[i].Account__r == null){
+                        newData[i] = {
+                            Name: result[i].Name,
+                            Week__c: result[i].Week__c,
+                            QC_Score__c: result[i].QC_Score__c,
+                            hero: "No Hero"
+                         };
+                    } else{
+
+                        newData[i] = {
+                            Name: result[i].Name,
+                            Week__c: result[i].Week__c,
+                            QC_Score__c: result[i].QC_Score__c,
+                            hero: result[i].Account__r.Name
+                         };
+                    }
+                }
+                //set the new data to the table
+                this.data = newData;
+            }
+        })
+        .catch(error => {
+            console.log('error: ', error);
+        });
+    }
+}
