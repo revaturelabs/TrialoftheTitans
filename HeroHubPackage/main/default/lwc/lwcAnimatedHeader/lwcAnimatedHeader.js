388fbf1b4333ff19606c783408423c65dd53a64d
Edmund Wong <edmundpunch5@gmail.com>
2021-08-27 10:53:56 -0400

    Fixed "lwcAnimatedHeader"

--- HeroHubPackage/main/default/lwc/lwcAnimatedHeader/lwcAnimatedHeader.js
+++ HeroHubPackage/main/default/lwc/lwcAnimatedHeader/lwcAnimatedHeader.js
@@ -3,39 +3,21 @@ import { LightningElement, api } from 'lwc';
 export default class AnimatedHeader extends LightningElement {
 
     @api leadTeams;
-
-    isAlchemy = false;
-    isAmplifire = false;
-    isSynergy = false;
-    isVanquish = false;
-
-    /*
-    currentTeam(){
-        if(leadTeams.Team[0][0] == 'Alchemy'){
-            isAlchemy = true;
-            isAmplifire = false;
-            isSynergy = false;
-            isVanquish = false;
-        }
-        else if(leadTeams.Team[0][0] == 'Amplifire'){
-            isAlchemy = false;
-            isAmplifire = true;
-            isSynergy = false;
-            isVanquish = false;
-        }
-        else if(leadTeams.Team[0][0] == 'Synergy'){
-            isAlchemy = false;
-            isAmplifire = false;
-            isSynergy = true;
-            isVanquish = false;
-        }
-        else if(leadTeams.Team[0][0] = 'Vanquish'){
-            isAlchemy = false;
-            isAmplifire = false;
-            isSynergy = false;
-            isVanquish = true;
-        }
+    
+    @api isAlchemy(){
+        return this.leadTeams.Team[0][0] == 'Alchemy';
     }
-    */
+
+    @api isAmplifire(){
+        return this.leadTeams.Team[0][0] == 'Amplifire';
+    }
+
+    @api isSynergy(){
+        return this.leadTeams.Team[0][0] == 'Synergy';
+    }
+
+    @api isVanquish(){
+        return this.leadTeams.Team[0][0] == 'Vanquish';
+    }
 
 }
