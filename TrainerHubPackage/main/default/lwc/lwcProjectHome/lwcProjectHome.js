f566a87337d5f0be67c74cabf7839b74736cf202
Timmy <miz1492@gmail.com>
2021-08-30 12:40:42 -0400

    Tim's finished components (lwcAddEquivalency, lwcProjectHome, lwcDynamicRow)

---
+++ TrainerHubPackage/main/default/lwc/lwcProjectHome/lwcProjectHome.js
@@ -0,0 +1,55 @@
+import retrieveProjects from "@salesforce/apex/ProjectHomeController.retrieveProjects";
+import { LightningElement, track, wire } from "lwc";
+
+export default class LwcProjectHome extends LightningElement {
+  // Project home is self contained so I didn't see a need to use any @api/@track decorators except on projectList
+  currentPage;
+  errorMessage;
+  selectedProjId;
+  @track projectList;
+
+  // wire the retrieveProjects method from the apex controller to get the project list
+  // store the data into the projectList property and any errors into the errorMessage property
+  @wire(retrieveProjects)
+  getProjectList({ error, data }) {
+    if (data) {
+      this.projectList = data;
+      this.errorMessage = undefined;
+    } else if (error) {
+      this.errorMessage = error;
+      this.projectList = undefined;
+    }
+  }
+
+  // simply changing the current page.
+  edit(event) {
+    this.selectedProjId = event.target.id;
+    this.currentPage = "dynamicRow";
+  }
+
+  assign(event) {
+    this.selectedProjId = event.target.id;
+    this.currentPage = "AssignProject";
+  }
+
+  addNewProject() {
+    this.currentPage = "NewProjectCreation";
+  }
+
+  // Used for if:true directive
+  get isHomePage() {
+    return this.currentPage == "homePage" ? true : false;
+  }
+
+  get isAssignProject() {
+    return this.currentPage == "AssignProject" ? true : false;
+  }
+
+  get isNewProjectCreation() {
+    return this.currentPage == "NewProjectCreation" ? true : false;
+  }
+
+  get isDynamicRow() {
+    return this.currentPage == "dynamicRow" ? true : false;
+  }
+}
