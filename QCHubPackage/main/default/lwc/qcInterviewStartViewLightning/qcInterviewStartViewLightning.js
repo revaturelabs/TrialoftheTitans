456c7d094d7908921636ab8d9c56af10846b4ddd
Justin Wu <justinandrewwu@gmail.com>
2021-08-31 15:35:47 -0600

    Update Events

--- QCHubPackage/main/default/lwc/qcInterviewStartViewLightning/qcInterviewStartViewLightning.js
+++ QCHubPackage/main/default/lwc/qcInterviewStartViewLightning/qcInterviewStartViewLightning.js
@@ -4,11 +4,12 @@ export default class QcInterviewStartViewLightning extends LightningElement {
     @api Cohort;
     @api WeekList;
     @api Week = 'Week 1';
-    connectedCallback(){
+    constructor(){
+        this.template.addEventListener('InterviewHeroEvent', StartInterview);
+    }
+    
+    StartInterview(){
         this.dispatchEvent(new CustomEvent("SetWeekEvent",{"Week" : this.Week}));
         this.dispatchEvent(new CustomEvent("UpdateStageEvent",{"StageName" : "Interview"}));
-    }
-    WeekSelect(){
-        this.Week = event.target.value;
     }
 }
