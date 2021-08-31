b9525b37b07ad177303c460240350f0263a30304
unknown <al.waisley@gmail.com>
2021-08-31 02:02:33 -0500

    Fixed assessmentCreationLWC

--- TrainerHubPackage/main/default/lwc/assessmentCreationLWC/assessmentCreationLWC.js
+++ TrainerHubPackage/main/default/lwc/assessmentCreationLWC/assessmentCreationLWC.js
@@ -5,9 +5,14 @@
 //  Description: JavaScript controller for assessmentCreation
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-({
-	SubmitClick : function(component, event, helper) {
+
+import { LightningElement } from 'lwc';
+
+export default class landingSkills extends LightningElement {
+    SubmitClick(event) {
         //Give an alert for successful record creation and hide component
         alert("Succesfully created record");
+        let assessmentForm = this.template.querySelector(`[data-id="assessment"]`);
+        assessmentForm.className = 'toggle-hide';
     }
-})
+}
