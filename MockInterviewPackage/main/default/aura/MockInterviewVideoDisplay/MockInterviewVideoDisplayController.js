8ff66f087bcb2ffdf9e11047e341eed004c5c88f
Brian McKay <turkeybones@att.net>
2021-09-03 13:35:56 -0500

    added missing methods referenced in documentation

--- MockInterviewPackage/main/default/aura/MockInterviewVideoDisplay/MockInterviewVideoDisplayController.js
+++ MockInterviewPackage/main/default/aura/MockInterviewVideoDisplay/MockInterviewVideoDisplayController.js
@@ -49,5 +49,44 @@
     videoCloseButton : function(component, event, helper){
         component.set("v.display", false);
         component.set("v.display4", true);
+    },
+    handleUploadFinished : function(component, event, helper) {
+        var uploadedFiles = event.getParam("files");
+        var documentId = uploadedFiles[0].documentId;
+        var fileName = uploadedFiles[0].name;
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "File "+fileName+" Uploaded successfully."
+        });
+        toastEvent.fire();
+        
+        $A.get('e.lightning:openFiles').fire({
+            recordIds: [documentId]
+        });
+        
+    },
+
+    displayFile : function(component, event, helper) {
+
+
+         let getFileURL = component.get("c.getContentId");
+         let contentId = component.get("v.recordId");
+
+         getFileURL.setParams({recordId:contentId});
+         console.log(contentId);
+         getFileURL.setCallback(this,function(response){
+
+            if(response.getState()==="SUCCESS"){
+                console.log(response.getReturnValue());
+                console.log("OMG");
+                component.set("v.fileUrl",response.getReturnValue());
+                console.log(component.get("v.fileUrl"));
+                component.set("v.display", true);
+            }
+         });
+
+         $A.enqueueAction(getFileURL);
+
     }
 })
