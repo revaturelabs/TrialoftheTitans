48e28ea55cbc9516fea51cf4f0c10a22f14ea34b
Benjamin Learn <38740610+learnbenlearn@users.noreply.github.com>
2021-09-07 14:18:56 -0400

    Revert "July 12th 2021 ctrl shift delete sam"

--- TrainerHubPackage/main/default/aura/CodingExamList/CodingExamListHelper.js
+++ TrainerHubPackage/main/default/aura/CodingExamList/CodingExamListHelper.js
@@ -1,24 +1,24 @@
 ({
     fetchData : function(component) {
-        let action = component.get('c.CodingExamList');
+        var action = component.get('c.CodingExamList');
         
-        let cohortId = component.get('v.CohortId');
+        var cohortId = component.get('v.CohortId');
 		action.setParams({
 			"cohortId": cohortId
         });
         
            action.setCallback(this, (function (response) {
-            let state = response.getState();
+            var state = response.getState();
             if (state === "SUCCESS") {
                 //have the data in the server in a variable
-                let data = response.getReturnValue();
+                var data = response.getReturnValue();
                 //for loop to set key for the coloumn with data
                 for( let i=0; i< data.length; i++ ){
                 	console.log(data[i]);
                 }
                  component.set('v.data', data);			        
             } else if (state === "ERROR") {
-                let errors = response.getError();
+                var errors = response.getError();
                 console.error(errors);
             }
          }));
