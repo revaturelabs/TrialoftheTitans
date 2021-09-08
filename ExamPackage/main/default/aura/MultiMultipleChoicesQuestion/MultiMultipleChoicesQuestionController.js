fc1c6cb42fe78f221476156091686211fdbb4b93
Alex Barnard <a1313barnard@gmail.com>
2021-09-08 10:55:25 -0400

    changes to Aura Components and Controllers

--- ExamPackage/main/default/aura/MultiMultipleChoicesQuestion/MultiMultipleChoicesQuestionController.js
+++ ExamPackage/main/default/aura/MultiMultipleChoicesQuestion/MultiMultipleChoicesQuestionController.js
@@ -2,8 +2,8 @@
 	fetchMultiChoices : function(component, event, helper) {
 		helper.fetchMultiChoices(component);
 	},
-    //this could get commented out, does nothing
-    handleChange: function (component, event) {
+    //this could get commented out, does nothing -Alex
+    //handleChange: function (component, event) {
         //alert(event.getParam('value'));
-    },
+    //},
 })
