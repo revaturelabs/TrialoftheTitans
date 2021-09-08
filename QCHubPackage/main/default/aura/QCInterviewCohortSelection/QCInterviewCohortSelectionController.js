bd2c2c8c92c77add6890e8bfd4293d8bb7236575
Michael <michaelerickson98@gmail.com>
2021-09-08 11:15:25 -0400

    Removed Unnecessary Comments

--- QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionController.js
+++ QCHubPackage/main/default/aura/QCInterviewCohortSelection/QCInterviewCohortSelectionController.js
@@ -1,5 +1,4 @@
 ({
-    // initialize creates columns for datatable
     init : function(component, event, helper) {
         helper.SetColumns(component);
         helper.getHeroes(component);
@@ -10,13 +9,11 @@
         helper.SetCohort(component, event);
     },
 
-    // row selection handles button to start interview
     handleRowSelection : function(component, event, helper) {
         helper.handleRowSelection(component, event)
     },
 
     LaunchInterview : function(component, event, helper){
-        console.log("LAUNCH INTERVIEW FUNCTION");
         helper.LaunchInterviewEvent(component, event);
     }
 })
