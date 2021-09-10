d192cde3bf09684c614d48edc4fd0d63b5479195
dataronio <davidshimer@yahoo.com>
2021-09-10 10:52:06 -0500

    adding apex controller for Portfolio Header , etc

--- HeroHubPackage/main/default/aura/PortfolioHeader/PortfolioHeaderController.js
+++ HeroHubPackage/main/default/aura/PortfolioHeader/PortfolioHeaderController.js
@@ -1,23 +1,31 @@
 ({
     doInit : function(component, event, helper) {
 
-        //let headerName = "<h2>" + "c.getUserName" + "</h2>"; Will be used when controller is written.
-        let headerNameTemp = "<h2>" + "USER NAME" + "</h2>"; //temporary username, need Apex controller to retrieve actual name.
+        let headerName = "<h2>" + "c.getUserName" + "</h2>"; 
+        //let headerNameTemp = "<h2>" + "USER NAME" + "</h2>"; //temporary username, need Apex controller to retrieve actual name.
 
-        //temporary loop to make mock titles to populate picklist. Should be retrieved from server in actual implementation.
+        let titleOne = "<h3>"+"Salesforce Administrator"+"</h3>"
+        let titleTwo = "<h3>"+"Salesforce Developer"+"</h3>"
+        let titleThree = "<h3>"+"Apex Developer"+"</h3>"
+        
         let titleList = [];
-        for(let i = 0; i < 10; i++) {
+           titleList.add('Salesforce Developer');
+           titleList.add('Salesforce Administrator');
+           titleList.add('Apex Developer');
+       
+       
+       // for(let i = 0; i < 10; i++) {
 
-            let option = {
-                "label" : "Temp Title " + i,
-                "value" : "Temp Title " + i.toString()
-            };
+       //     let option = {
+       //         "label" : "Temp Title " + i,
+       //         "value" : "Temp Title " + i.toString()
+       //     };
 
-            titleList.push(option);
+       //     titleList.push(option);
 
-        }
+       // }
 
-        component.set("v.userName", headerNameTemp); //change to headerName after Apex controller is made.
+        component.set("v.userName", headerName); //change to headerName after Apex controller is made.
         component.set("v.titleList", titleList);
 
     },
