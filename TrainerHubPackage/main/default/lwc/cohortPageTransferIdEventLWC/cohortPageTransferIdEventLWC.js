4ec98d1b35e0df5dff26eac19a354db144e560be
Korhan-Bleu <stephen.pryor@revature.net>
2021-09-13 11:08:39 -0500

    Revert "Merge branch 'main' into July-12th-2021-CTRL-Shift-Delete-StephenPryor"
    This reverts commit 73a1b31ecf6879c5175590560b26c195bba540b0, reversing
    changes made to 596ab87deccf837eb10da41a7f3075f5d5abdb80.

--- TrainerHubPackage/main/default/lwc/cohortPageTransferIdEventLWC/cohortPageTransferIdEventLWC.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CohortPageTransferIdEventLWC extends LightningElement {
-    CohortPageTransferIdEvent(){
-        this.dispatchEvent(new CustomEvent('CohortPageTransferIdEventLWC'));
-    }
-}
