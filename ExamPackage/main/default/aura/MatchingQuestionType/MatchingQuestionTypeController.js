e4cc6d618d0470456b1811d1f3b30fbb6dbbb31a
MarkusMetli <81584037+MarkusMetli@users.noreply.github.com>
2021-09-13 14:10:34 -0700

    all question types other than matching working

--- ExamPackage/main/default/aura/MatchingQuestionType/MatchingQuestionTypeController.js
+++ ExamPackage/main/default/aura/MatchingQuestionType/MatchingQuestionTypeController.js
@@ -3,10 +3,18 @@
         helper.loadHelper(component);
 
     },
-    // returns answer to the examinterview component
     answer : function(cmp) {
-        
-        var answer = cmp.get("v.answers");
-        return answer;
+        // retrieves array object of selected values
+        var answer = cmp.find('test').get("v.value");
+        console.log(answer);
+        //converts all answers from array into a single string
+        var answerString = "";
+        for(let i = 0; i < answer.length - 1; i++){
+            answerString += answer[i] + "||" ;
+        }
+        answerString += answer[answer.length - 1];
+
+        console.log(answerString);
+        return answerString;
     }
 })
