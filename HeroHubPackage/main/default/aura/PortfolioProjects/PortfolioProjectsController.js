c4fa9edc866f481dea3df860f790cae12467a708
murrai.scanlon@gmail.com <murrai.scanlon@gmail.com>
2021-09-14 15:47:09 -0500

    Add project controller test

--- HeroHubPackage/main/default/aura/PortfolioProjects/PortfolioProjectsController.js
+++ HeroHubPackage/main/default/aura/PortfolioProjects/PortfolioProjectsController.js
@@ -1,7 +1,54 @@
 ({
+    doInit : function(component,event, helper){
+        helper.showProjects(component, event); 
+    },
     
-    //Nothing fancy here
-    doInit : function(component, event, helper){
-        helper.getProjects(component, event);
-    }
+    // Save new project information and display on screen, when a user fills out the information    
+    doSave : function(component, event, helper){
+			var action = component.get("c.createProject");
+            var projects = component.get("v.projects");
+            action.setParams({"projObj" : component.get('v.projectObj'),
+                              "updateParam" : false});
+            action.setCallback(this, function(data){
+                component.set('v.projectId', data.getReturnValue());
+                projects.splice(0,0,component.get('v.projectObj'));
+                component.set("v.projects", projects);
+                
+            });
+            
+         $A.enqueueAction(action);
+            
+    },
+    		// Toggle the Edit button
+            doEdit : function(component, event, helper){
+			component.set('v.isEdit',false);
+   
+            
+    },
+    
+    		// Allow user to Cancel and Edit
+    		doCancel : function(component, event, helper){
+			component.set('v.isEdit',true);
+   
+            
+    },
+    
+            // Allow user to Save an Edit to an existing project. THIS IS NOT CURRENTLY SAVING 
+            // EDITS TO THE DATABASE, ONLY TO THE VIEWING PAGE.
+    		doReSave : function(component, event, helper){
+			component.set('v.isEdit',true);      
+               
+            var action = component.get("c.createProject");
+			var projects = component.get("v.projects");
+            action.setParams({"projObj" : component.get('v.projectObj'),
+                              "updateParam" : true});
+            action.setCallback(this, function(data){
+                component.set('v.projectId', data.getReturnValue());
+                component.set("v.projects", projects);
+                console.log('resave projects: ' + projects[0].Description__c);  
+                
+            });
+    	$A.enqueueAction(action);
+        
+    },
 })
