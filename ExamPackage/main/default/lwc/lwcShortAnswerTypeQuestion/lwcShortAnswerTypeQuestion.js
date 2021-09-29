15a3bc339f8e6e2d29b3dd1a4cef04052fb9aebf
John L <johnny@example.com>
2021-09-29 14:32:35 -0500

    started converting aura component, started setup of javascript, added meta data code, run diff file command on appswitcher because of deployment

--- ExamPackage/main/default/lwc/lwcShortAnswerTypeQuestion/lwcShortAnswerTypeQuestion.js
+++ ExamPackage/main/default/lwc/lwcShortAnswerTypeQuestion/lwcShortAnswerTypeQuestion.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class LwcShortAnswerTypeQuestion extends LightningElement {}
+export default class LwcShortAnswerTypeQuestion extends LightningElement {
+    @api ShortQuestion;
+    answer() {
+        
+    }
+}
