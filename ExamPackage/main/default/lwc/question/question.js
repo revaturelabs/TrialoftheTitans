4f13ebbfb144b6ce3e9664e07d711f156c39d264
apsoftwaredev <1483796+apsoftwaredev@users.noreply.github.com>
2021-09-29 02:08:18 +0000

    added question

---
+++ ExamPackage/main/default/lwc/question/question.js
@@ -0,0 +1,14 @@
+/**
+ * @description       : 
+ * @author            : Daniel Boice
+ * @group             : 
+ * @last modified on  : 09-29-2021
+ * @last modified by  : Daniel Boice
+ * Modifications Log
+ * Ver   Date         Author         Modification
+ * 1.0   09-29-2021   Daniel Boice   Initial Version
+**/
+
+import { LightningElement } from 'lwc';
+
+export default class Question extends LightningElement {}
