aaafe4292e1468e3630fff2ddc23f5d9546b9377
Heejin Wee <heejinw07@gmail.com>
2021-10-04 17:14:38 -0400

    lwc QC Interview Component

---
+++ QCHubPackage/main/default/lwc/lwcQCInterview/lwcQCInterview.js
@@ -0,0 +1,55 @@
+import { LightningElement ,api} from 'lwc';
+
+export default class LwcQCInterview extends LightningElement {
+
+@api Stage='Start';
+//@api CompanyAuthorizedDate;
+
+@api CurrentHero;
+@api WeekList=[];
+@api Week;
+@api CurrentQAList;
+@api Cohort;
+@api pageReference;
+
+
+get startView(){
+    return this.Stage=='Start' ? true : false;  
+}
+
+get interviewQA(){
+    return this.Stage=='Interview' ? true : false;
+}
+
+get interviewEnd(){
+    return this.Stage=='End' ? true : false;
+}
+
+//LoadSessionData()
+connectedCallback(){
+ /*   let myPageRef = pageReference;
+    let sessionCohort = myPageRef.state.c__Cohort;
+    this.Cohort = sessionCohort; */
+}
+    
+
+UpdateStage(event){
+    this.Stage= event.detail.value;
+    console.log(event);
+}
+
+SetHero(event){
+    this.CurrentHero=event.detail.value;
+}
+SetWeek(event){
+    this.Week=event.detail.value;
+}
+
+AddQuestionAnswer(event){
+    //Converting: let questionAnswer = event.getParam("QA"); where detail="QA"
+    let questionAnswer=event.detail.value;
+
+    this.CurrentQAList.push(questionAnswer);
+}
+    
+}
