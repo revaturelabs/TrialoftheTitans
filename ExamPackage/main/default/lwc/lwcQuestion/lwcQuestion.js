4debb9c20a48210155478092bd85a34c075e19f8
apsoftwaredev <1483796+apsoftwaredev@users.noreply.github.com>
2021-10-05 00:33:57 +0000

    updated the combo box setting to work with matchin

--- ExamPackage/main/default/lwc/lwcQuestion/lwcQuestion.js
+++ ExamPackage/main/default/lwc/lwcQuestion/lwcQuestion.js
@@ -2,7 +2,7 @@
  * @description       : 
  * @author            : Daniel Boice
  * @group             : 
- * @last modified on  : 10-03-2021
+ * @last modified on  : 10-05-2021
  * @last modified by  : Daniel Boice
  * Modifications Log
  * Ver   Date         Author         Modification
@@ -32,10 +32,17 @@ export default class LwcQuestion extends LightningElement {
     displayInput=false;
     displayCheckboxGroup=false;
     displayComboBox=false;
-    optionsForComboBox_ =[];
+    optionsForComboBox =[];
     radioCheckboxGroupComboBoxOptions_=[];
-    answersForComboBox_=[];
-    
+    answersForComboBox=[];
+    comboBoxAnswersProvided={choice1:'Test One', choice2:'Test two', choice3:'Test three'};
+    get options() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
       value = '';
     @api
     handleSetAnswer(answer){
@@ -43,14 +50,68 @@ export default class LwcQuestion extends LightningElement {
       //  based on lightning-textarea, lightning-combobox
       //  lightning-input, lightning-radio-group or lightning-combobox
       // could be done in separate components if passed in when the question updates
-      
+       
         this.setDisplayQuestionTypeBoolValues();
         if ( typeof this.question!=='undefined' && this.question){
             if( this.question.Options__c && !(this.displayTextarea||this.displayInput)){
-                if(this.displayComboBox&&this.question.Correct_Answer_s__c){
-                this.answersForComboBox=this.createOptionsArrayFromString(this.question.Correct_Answer_s__c);
-                this.optionsForCombobox=this.question.Options__c.toString().split('||');
-                this.answer=answer.toString().split('||');
+                if(this.displayComboBox && this.question.Correct_Answer_s__c){
+                    //this.answersForComboBox=this.createOptionsArrayFromString(this.question.Correct_Answer_s__c);
+                    this.answersForComboBox=this.createOptionsArrayFromString(this.question.Correct_Answer_s__c);
+                    this.answersForComboBox.push({label:'not matched',value:'not matched'})
+                    
+                    this.comboBoxAnswersProvided ={};
+               
+                    var test = this.question.Options__c.split('||');
+                    console.log(test);
+                    this.optionsForCombobox=test;
+                  
+                    for(let i = 0;i < test.length;i++){
+                        console.log(test[i]);
+                        
+                        this.comboBoxAnswersProvided[test[i]]='not matched';
+                    }
+                    this.question.Options__c.split('||').forEach(opt =>{
+                        console.log(opt);
+                        this.comboBoxAnswersProvided[opt]='not matched';
+                    });
+                   
+                    if(typeof answer==='string' && answer.length){
+
+                        var answerOptionList =answer.toString().split('||') ;
+                       for(let i = 0;i<answerOptionList.length;i++){
+                            let objKeysComboBoxOptions =Object.keys(this.comboBoxAnswersProvided);
+                            if(i<objKeysComboBoxOptions.length){
+                                // console.log("val is "+this.comboBoxAnswersProvided[objKeysComboBoxOptions[i]]);
+                                // console.log('answer list value is ');
+                                // console.log(answerOptionList[i]);
+                                this.comboBoxAnswersProvided[objKeysComboBoxOptions[i]]=answerOptionList[i] ;
+
+                                // console.log('answeroptionlist is '+ answerOptionList[i]);
+                                // console.log(objKeysComboBoxOptions[i]);
+                                // const comboOptionComponent = this.shadowRoot.querySelector(`lightning-combobox[name="${objKeysComboBoxOptions[i]}"]`);
+
+                                //having difficulty updating the combo boxes when returning to them. but answers are saved and loaded. Just the selection lists are not showing selected.
+                                // console.log("checking");
+                                this.template.querySelectorAll('lightning-combobox').forEach(ele => {
+                                    if(ele.name == objKeysComboBoxOptions[i]){
+                                     
+                                        console.log("conboboxoption component found");
+                                        console.log('setting to ' + answerOptionList[i]);
+                                        comboOptionComponent.value= answerOptionList[i];
+                                    }
+                                });
+                                // if(comboOptionComponent ){
+                                   
+                                   
+                                // }
+                                // else{
+                                //     console.log(`lightning-combobox[data-name="${objKeysComboBoxOptions[i]}"]`)
+                                //     console.log('did not find the component')
+                                // }
+                            }
+                        }
+                           
+                    }
                 }
                 else{
                     this.radioCheckboxGroupComboBoxOptions= this.createOptionsArrayFromString(this.question.Options__c);
@@ -62,20 +123,20 @@ export default class LwcQuestion extends LightningElement {
                     answerString=answerString.length?answerString.slice(0,-1):'';
                     this.answer=answerString;
                 }
-            }
-            else{
-                //it is a textarea, number
-                this.answer = answer;
-            }
         }
-        this.displayAnswerGiven  = answer;
+        else{
+            //it is a textarea, number
+            this.answer = answer;
+        }
+    }
+    this.displayAnswerGiven  = answer;
     }
     
     // takes string and formats for displaying in control 
     createOptionsArrayFromString(optionsString){
         if(optionsString.length){
-           let options = optionsString.split('||');
-            let optionsArray = [];
+           var options = optionsString.split('||');
+            var optionsArray = [];
             for (let i = 0; i< options.length; i++) {
                 let myObject = {
                     'label': `${options[i]}`,
@@ -83,6 +144,8 @@ export default class LwcQuestion extends LightningElement {
                 };
                 optionsArray.push(myObject)
             }
+            console.log("optionsArray in create")
+            console.log(optionsArray);
            return optionsArray;
         }
     }
@@ -93,24 +156,28 @@ export default class LwcQuestion extends LightningElement {
     set radioCheckboxGroupComboBoxOptions(myoptions){
         this.radioCheckboxGroupComboBoxOptions_ = myoptions;
     }
-    get optionsForCombobox(){
-        return this.optionsForComboBox_;
-    }
-    set optionsForCombobox(myoptions){
-        this.optionsForComboBox_= myoptions;
-    }
-    get answersForComboBox(){
-       return this.answersForComboBox_;
-    }
-    set answersForComboBox(myanswers){
-        this.optionsForComboBox_=myanswers;
-    }
+    // get optionsForCombobox(){
+    //     return this.optionsForComboBox_;
+    // }
+    // set optionsForCombobox(myoptions){
+    //     this.optionsForComboBox_= myoptions;
+    // }
+    // get answersForComboBox(){
+    //    return this.answersForComboBox_;
+    // }
+    // set answersForComboBox(myanswers){
+    //     this.answersForComboBox=myanswers;
+    // }
 
     //sends up the answer in the correct format based on question type
     handleInputAnswer(event) {
       let typeOfValue = typeof event.target.value;
       var stringToSend='';
+      console.log(typeOfValue);
+       
       if(typeOfValue==="object"){
+          console.log(event.target.value);
+        
           let obj=event.target.value;
           let objLength=Object.keys(obj).length;
             for(let i = 0; i< objLength; i++){
@@ -118,11 +185,39 @@ export default class LwcQuestion extends LightningElement {
             }
             stringToSend=stringToSend.slice(0,-2);
       }else if(typeOfValue==='string'){
-            stringToSend=event.target.value?event.target.value:'';
+            
+            if (this.displayComboBox ){
+               
+                
+                  //formatting the combo box string to store for matching
+                    this.comboBoxAnswersProvided[event.target.name]=event.target.value;
+                    console.log(this.comboBoxAnswersProvided);
+                    var createAnswerString="";
+                
+                  
+                    for(let i = 0;i<Object.keys(this.comboBoxAnswersProvided).length;i++){
+                     
+                        createAnswerString+=this.comboBoxAnswersProvided[Object.keys(this.comboBoxAnswersProvided)[i]]+'||';
+                    }
+                    // Object.keys(this.comboBoxAnswersProvided).forEach(val =>{
+                        
+
+                       
+
+                    // });
+                    createAnswerString= createAnswerString.length>1? createAnswerString.slice(0,-2):createAnswerString;
+                    console.log(createAnswerString);
+                    stringToSend=createAnswerString;
+            }
+            else{
+
+                stringToSend=event.target.value?event.target.value:'';
+            }
+            
       }
 
       //event.preventDefault();
-      this.displayAnswerGiven = event.target.value;
+      this.displayAnswerGiven = stringToSend;
       // Creates the event with the answer data.
       const answerEvent = new CustomEvent('answerupdated', { detail: stringToSend });
       // Dispatches the event.
