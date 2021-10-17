02ebe205febd46fb14ae431fce7240c4daf8ccb5
William Rembish <wrembish@udel.edu>
2021-10-17 11:55:49 -0400

    Finished testing for lwcMatchingQuestionType Component

--- ExamPackage/main/default/lwc/lwcMatchingQuestionType/__tests__/lwcMatchingQuestionType.test.js
+++ ExamPackage/main/default/lwc/lwcMatchingQuestionType/__tests__/lwcMatchingQuestionType.test.js
@@ -1,16 +1,23 @@
 import { createElement } from 'lwc';
 import LwcMatchingQuestionType from 'c/lwcMatchingQuestionType';
 
+/*
+    Created By: William Rembish
+    date: 10/17/2021
+    test coverage: 100%
+*/
 describe('c-lwc-matching-question-type', () => {
+    // after each test, reset the DOM
     afterEach(() => {
-        // The jsdom instance is shared across test cases in a single file so reset the DOM
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
     });
 
+    // declare the element variable
     let element;
 
+    // before each test, set element to be an instance of the lwcMatchingQuestionType component
     beforeEach(() => {
         element = createElement('c-lwc-matching-question-type', {
             is: LwcMatchingQuestionType
@@ -18,6 +25,7 @@ describe('c-lwc-matching-question-type', () => {
     });
 
     it('Testing if the header displays the correct information', () => {
+        // append the element to the DOM
         document.body.appendChild(element);
         // check the paragraph elements on the page to make sure they display what they are supposed to
         const paragraph = Array.from(element.shadowRoot.querySelectorAll("p"));
@@ -27,9 +35,37 @@ describe('c-lwc-matching-question-type', () => {
         expect(element.questionprompt).toBe("can you come here");
     });
 
-    it('Test', () => {
-        element.options = "h||e||l||l||o";
-        element.answers = [{label: "hello", value: "world"}]
+    it('Testing if the value is properly changed in the onchange method', () => {
+        // before appending the element to the DOM, set the values of the options and answers variables
+        element.options = "h||e||y";
+        element.answers = "y||o||u";
+        // append the element to the DOM
         document.body.appendChild(element);
+        // check to make sure each variable in the element is equal to what is expected
+        expect(element.answerChoice).toBe(undefined);
+        expect(element.options).toStrictEqual(['h','e','y']);
+        expect(element.answers).toStrictEqual([{label: 'y', value: 'y'},{label: 'o', value: 'o'},
+        {label: 'u', value: 'u'}]);
+        expect(element.answer()).toBe(undefined);
+
+        // simulated selecting an answer choice and 'dispatch' the event
+        let combobox = element.shadowRoot.querySelector("lightning-combobox");
+        combobox.dispatchEvent(new CustomEvent("change", {
+            detail: {
+                value: "y"
+            }
+        }));
+        // check to make sure the onchange function performed as expected
+        return Promise.resolve().then(() => {
+            const outputElement = element.shadowRoot.querySelector("lightning-combobox");
+            expect(outputElement.value).toStrictEqual([{"label": "y", "value": "y"}, {"label": "o", "value": "o"}, {"label": "u", "value": "u"}]);
+        })
     });
+
+    it('Test if only option is selected', () => {
+        // checks the last line of code that isn't reached by the other tests
+        element.options = "h||e||y";
+        document.body.appendChild(element);
+        expect(element.answers).toBe(undefined);
+    })
 });
