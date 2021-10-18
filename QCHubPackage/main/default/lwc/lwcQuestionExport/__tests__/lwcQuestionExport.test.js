1535863a59df7cda0f963ff5f49b78bfd58f3231
William Rembish <wrembish@udel.edu>
2021-10-18 13:02:26 -0400

    finished testing for lwcQuestionExport component

--- QCHubPackage/main/default/lwc/lwcQuestionExport/__tests__/lwcQuestionExport.test.js
+++ QCHubPackage/main/default/lwc/lwcQuestionExport/__tests__/lwcQuestionExport.test.js
@@ -3,6 +3,7 @@ import LwcQuestionExport from 'c/lwcQuestionExport';
 import getQCQuestions from '@salesforce/apex/QCQuestionExportApexController.getQCQuestions';
 
 const mockGetQCQuestion = require('./data/getQCQuestions.json');
+const mockGetQCQuestionNoReturns = require('./data/getQCQuestionsNoReturn.json');
 
 // mocking imperative call to getQCQuestions apex method
 jest.mock(
@@ -54,6 +55,54 @@ describe('c-lwc-question-export', () => {
 
         // Wait for any asynchronous DOM updates
         await flushPromises();
+
+        // test to make sure the connected callback function properly calls the apex method and assigns the values to the questionList variable
         expect(element.questionList).toStrictEqual([{Name: 'Hello', Question_Body__c: 'World', Id: 'a0KJ000000JaLP4MAN'}, {Name: 'Test', Question_Body__c: 'Test Question', Id: 'a0KJ000000JaLTwMAN'}]);
+    });
+
+    it('Test button press on no records returned', async () => {
+        // creates a jest spy to check if something is console logged
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        // Assign mock value for resolved Apex promise
+        getQCQuestions.mockResolvedValue(mockGetQCQuestionNoReturns);
+
+        // append the element to the DOM with no returns
+        document.body.appendChild(element);
+
+        // Wait for any asynchronous DOM updates
+        await flushPromises();
+
+        // make sure that the questionList is empty
+        expect(element.questionList).toStrictEqual([]);
+
+        // simulate onclick for the button to download the csv
+        const button = element.shadowRoot.querySelector("lightning-button");
+        button.click();
+
+        // test to make sure that the console logs that there were no values to download in the csv
+        expect(consoleSpy).toHaveBeenCalledWith('No values in the csv');
+    });
+
+    it('Test button press on records returned', async () => {
+        // create a jest spy to check if the download link is created
+        const createElementSpy = jest.spyOn(document, 'createElement');
+
+        // Assign mock value for resolved Apex promise
+        getQCQuestions.mockResolvedValue(mockGetQCQuestion);
+
+        // append the element to the DOM
+        document.body.appendChild(element);
+
+        // Wait for any asynchronous DOM updates
+        await flushPromises();
+
+        // test to make sure the connected callback function properly calls the apex method and assigns the values to the questionList variable
+        expect(element.questionList).toStrictEqual([{Name: 'Hello', Question_Body__c: 'World', Id: 'a0KJ000000JaLP4MAN'}, {Name: 'Test', Question_Body__c: 'Test Question', Id: 'a0KJ000000JaLTwMAN'}]);
+
+        const button = element.shadowRoot.querySelector("lightning-button");
+        button.click();
+
+        expect(createElementSpy).toHaveBeenCalledWith("a");
     });
 });
