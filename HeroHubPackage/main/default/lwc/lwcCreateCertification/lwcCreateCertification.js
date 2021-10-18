491cac4918d0734aa95daf9e4a4e06d9cacbf9dc
Liam Hunt <liamhunt1789@gmail.com>
2021-10-18 00:22:16 -0400

    Minor changes to CreateCertification lwc

--- HeroHubPackage/main/default/lwc/lwcCreateCertification/lwcCreateCertification.js
+++ HeroHubPackage/main/default/lwc/lwcCreateCertification/lwcCreateCertification.js
@@ -5,12 +5,12 @@ export default class LwcCreateCertification extends LightningElement {
     //Show that record has been added to the database
     const evt = new ShowToastEvent({
       title: "Successful Creation",
-      message: "Question was successfully Created",
+      message: "Certification was successfully added!",
       variant: "success"
     });
     this.dispatchEvent(evt);
 
-    // Set the recordId to null so a new question can be entered
+    // Set the recordId to null so a new certification can be entered
     const editForm = this.template.querySelector("lightning-record-form");
     editForm.recordId = null;
   }
