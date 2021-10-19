58cf5cc1f7a589953e3ae7c1b9d5e8baa7537321
Julia Weakley <Julia218@me.com>
2021-10-18 22:46:47 -0600

    lwcEditCert files updated and test files created

---
+++ HeroHubPackage/main/default/lwc/lwcEditCert/__tests__/lwcEditCert.test.js
@@ -0,0 +1,60 @@
+/* 
+    Author: Julia Weakley
+    Date Last Modified: 10/18/2021
+    Description:  
+        Tests for lwcEditCert component. 100% code coverage
+*/ 
+
+import { createElement } from "lwc";
+import LwcEditCert from 'c/lwcEditCert';
+
+describe('c-lwc-Edit-Cert', ()=> {
+    afterEach(()=> {
+        while(document.body.firstChild)
+        {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+        let element = createElement('c-lwc-Edit-Cert',{
+            is: LwcEditCert
+
+        });
+
+    
+    // test checks that form appears 
+    it("editForm", ()=>{
+ 
+      
+    document.body.appendChild(element);
+
+ 
+    const form = element.shadowRoot.querySelector("lightning-record-view-form");
+    expect(form.recordId).toBeFalsy();
+
+    // simulate the submit event
+    form.dispatchEvent(new CustomEvent("submit"));
+
+    // check that recordId is reset to null
+     return Promise.resolve().then(() => {
+        expect(form.recordId).toBeFalsy;
+        });
+
+    });
+
+    // Test checks that form gets submited
+    it("editFormEvent", ()=>{
+        document.body.appendChild(element);
+
+        const form = element.shadowRoot.querySelector('lightning-record-view-form');
+       // mock submit function for Lightning-record-edit-form
+        form.submit = jest.fn(); 
+
+        form.dispatchEvent(new CustomEvent('submit'));
+
+        return Promise.resolve().then(() => {
+        expect(form.submit).toBeTruthy(); 
+        
+        });
+    });
+});
--- HeroHubPackage/main/default/lwc/lwcEditCert/lwcEditCert.js
+++ HeroHubPackage/main/default/lwc/lwcEditCert/lwcEditCert.js
@@ -1,37 +1,42 @@
+/*
+    Author: Julia Weakley
+    Date Last Modified: 10/18/2021
+    Description:  
+        Js file to allow functionality for edit their certifications information
+
+*/
 import { LightningElement, wire, track, api } from 'lwc';
 import Id from '@salesforce/user/Id';
-
-
 import NAME from '@salesforce/schema/User.Name';
-import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
-import Certifications from '@salesforce/apex/getCertifications.Certifications'
+
 const fields =[NAME];
 
 export default class LwcEditCert extends LightningElement 
 {
+   
     @api recordId; 
-    userId = Id;
-    @wire (getRecord, {recordId: '$userId', fields}) user; 
     
-
-    @api edit= false; 
+    // current user id
+    userId = Id;
+    // helps determine what record form should be shown 
+    @api edit= false;
+    // certification record id that was passed from the parent  
     @api certid; 
     @api certimage; 
-   
-
-    
- handleClick(event)
-  {
-     if(this.edit == true)
+ 
+   // event to handle button clicks 
+   handleClick(event)
+   {
+     // if current value of edit is true change it to false 
+     // else set edit to true
+      if(this.edit == true)
       {
          this.edit =false; 
-      }
+      } // end of if
       else
       {
         this.edit = true; 
-      } 
-      console.log('Clicked ' );
-  }
-
-    
+      } // end of else
+     
+   } // end of button functionality   
 }
