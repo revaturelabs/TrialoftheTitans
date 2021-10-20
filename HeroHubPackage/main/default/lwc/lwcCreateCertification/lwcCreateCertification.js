eb660564c0500dda17fac824ed007485b237dadb
JuliaWeakley <54649800+JuliaWeakley@users.noreply.github.com>
2021-10-20 14:20:43 -0600

    Update lwcCreateCertification.js

--- HeroHubPackage/main/default/lwc/lwcCreateCertification/lwcCreateCertification.js
+++ HeroHubPackage/main/default/lwc/lwcCreateCertification/lwcCreateCertification.js
@@ -8,8 +8,6 @@
 
 */
 
-*/
-
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 export default class LwcCreateCertification extends LightningElement {
