e875b2229bfce1a0520241ce8d7bf544a5be8e64
Julia Weakley <Julia218@me.com>
2021-10-21 14:59:10 -0600

    tests updates

--- HeroHubPackage/main/default/lwc/lwcDisplayCert/__tests__/lwcDisplayCert.test.js
+++ HeroHubPackage/main/default/lwc/lwcDisplayCert/__tests__/lwcDisplayCert.test.js
@@ -3,11 +3,34 @@
     Date Last Modified: 10/18/2021
     Description:  
         Tests for lwcEditCert component. 100% code coverage
-*/ 
-
+*/
+import LwcDisplayCert from "c/lwcDisplayCert";
 import { createElement } from "lwc";
-import LwcDisplayCert from 'c/lwcDisplayCert';
+import { registerApexTestWireAdapter } from "@salesforce/wire-service-jest-util";
+import { getCRecord } from "@salesforce/apex/getCertifications.Certifications";
 
+const getList = require("./data/getRecord.json");
+//const getCertsAdapter = registerApexTestWireAdapter(getCRecord);
+describe("c-lwc-Display-Cert", () => {
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+    jest.clearAllMocks();
+  });
+
+  it("shows records", () => {
+    const element = createElement("c-lwcDisplayCert", {
+      is: LwcDisplayCert
+    });
+    element.certs = getList;
+    document.body.appendChild(element);
+  });
+});
+/*
+const mockRecord = require('./data/getRecord.json');
+const mockNoRecords = require('./data/getNoRecords.json');
+const getRecordAdapter = registerApexTestWireAdapter(getCRecord);
 describe('c-lwc-Display-Cert', ()=> 
 {
     afterEach(()=> 
@@ -17,16 +40,27 @@ describe('c-lwc-Display-Cert', ()=>
             document.body.removeChild(document.body.firstChild);
         }
     });
+  
+    describe('getCertifications @wire data', ()=>{ 
+        const element = createElement('c-lwc-Display-Cert',{
+                is:LwcDisplayCert
+            });
+        it('renders records', ()=>{
+           
+            document.body.appendChild(element);
 
-   
+            getRecordAdapter.emit(mockRecord);
 
-    
-    // test checks that form appears 
-    it("display", ()=>{
-        const wrapper = shallow(<lwcDisplayCert />); 
-        expect(wrapper.find(lwcEditCert).length).toEqual(1);
+            return Promise.resolve().then(()=>{
+                const nameElement = element.shadowRoot.querySelectorAll('lightning-layout-item'); 
+                expect(certificationElements.length).toBe(mockRecord.length); 
+
+            });
+
+        });
+
     });
-
-    
+ 
    
 });
+*/
--- HeroHubPackage/main/default/lwc/lwcDisplayCert/lwcDisplayCert.js
+++ HeroHubPackage/main/default/lwc/lwcDisplayCert/lwcDisplayCert.js
@@ -1,22 +1,11 @@
-import { LightningElement, wire, api, track } from 'lwc';
-import Id from '@salesforce/user/Id';
+import { LightningElement, wire, api, track } from "lwc";
+import Id from "@salesforce/user/Id";
 
+import NAME from "@salesforce/schema/User.Name";
+import { getRecord } from "lightning/uiRecordApi";
+import Certifications from "@salesforce/apex/getCertifications.Certifications";
+const fields = [NAME];
 
-import NAME from '@salesforce/schema/User.Name';
-import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
-import Certifications from '@salesforce/apex/getCertifications.Certifications'
-const fields =[NAME];
-export default class DisplayCert extends LightningElement 
-{
-  
-    @api recordId; 
-    userId = Id;
-    @wire (getRecord, {recordId: '$userId', fields}) user; 
-    
-    @wire(Certifications) certs; 
-
-
-   
-
-    
+export default class DisplayCert extends LightningElement {
+  @wire(Certifications) certs;
 }
