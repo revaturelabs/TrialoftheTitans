3cbf7c72cce1460fe9061f21207b1c31a1709098
Mfernan4 <89104222+Mfernan4@users.noreply.github.com>
2021-10-22 01:03:07 -0400

    Finished ExamResultsPage Testing
    :)

--- ExamPackage/main/default/lwc/lwcExamResultsPoolPage/__tests__/LwcExamResultsPoolPage.test.js
+++ ExamPackage/main/default/lwc/lwcExamResultsPoolPage/__tests__/LwcExamResultsPoolPage.test.js
@@ -1,11 +1,16 @@
 import { createElement } from 'lwc';
 import LwcExamResultsPoolPage from 'c/LwcExamResultsPoolPage';
 
-import getQCQuestions from '@salesforce/apex/QCQuestionExportApexController.getQCQuestions';
+import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 
-const mockGetQCQuestion = require('./data/correct.json');
-const mockGetQCQuestionNoReturns = require('./data/questions.json');
+import GetAllExamResultPoolQuestions from '@salesforce/apex/ExamResultsPoolPageAuraController.GetAllExamResultPoolQuestions';
+import GetCorrectExamResultPoolQuestions from '@salesforce/apex/ExamResultsPoolPageAuraController.GetCorrectExamResultPoolQuestions';
 
+const mockGetCorrectExamResultPoolQuestions = require('./data/correct.json');
+const mockGetAllExamResultPoolQuestions = require('./data/questions.json');
+
+const getCorrectListAdapter = registerApexTestWireAdapter(GetCorrectExamResultPoolQuestions);
+const getAllListAdapter = registerApexTestWireAdapter(GetAllExamResultPoolQuestions);
 
 
 jest.mock(
@@ -35,37 +40,61 @@ describe('c--lwc-exam-results-pool-page', () => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+        jest.clearAllMocks();
     });
 
     let element;
 
     beforeEach(() => {
-        element = createElement('c-lwc-q-c-interview-header', {
-            is: LwcQCInterviewHeader
+        element = createElement('c--lwc-exam-results-pool-page', {
+            is: LwcExamResultsPoolPage
         });
         document.body.appendChild(element);
+        element.examResultId = "a090R00000DPqc6QAD";
+
     });
 
-    it('Test to see if start is active component on init', () => { 
-        let active = element.shadowRoot.querySelector(".active");
-        expect(active.textContent).toBe("Start");
-    });
+    async function flushPromises() {
+        return Promise.resolve();
+      }
 
-
-    it('Test connectedCallback', async () => {
-
-        // Assign mock value for resolved Apex promise
-        getQCQuestions.mockResolvedValue(mockGetQCQuestion);
-
-        // append the element to the DOM
-        document.body.appendChild(element);
-
-        // Wait for any asynchronous DOM updates
+    it('Test Correct array size from mock data wire', async () => {
+        getCorrectListAdapter.emit(mockGetCorrectExamResultPoolQuestions);
         await flushPromises();
-
-        // test to make sure the connected callback function properly calls the apex method and assigns the values to the questionList variable
-        expect(element.questionList).toStrictEqual([{Name: 'Hello', Question_Body__c: 'World', Id: 'a0KJ000000JaLP4MAN'}, {Name: 'Test', Question_Body__c: 'Test Question', Id: 'a0KJ000000JaLTwMAN'}]);
+        expect(element.correctQuestions.length).toBe(2);
     });
 
+    it('Test All array size from mock data wire', async () => {
+        getAllListAdapter.emit(mockGetAllExamResultPoolQuestions);
+        await flushPromises();
+        expect(element.ExamResultPoolQuestions.length).toBe(4);
+    });
+
+    it('Test exam percentage off mock data wire executed in All then Correct order', async () => {
+        getAllListAdapter.emit(mockGetAllExamResultPoolQuestions);
+        getCorrectListAdapter.emit(mockGetCorrectExamResultPoolQuestions);
+        await flushPromises();
+        let text = element.shadowRoot.querySelector(".wrapper");
+        let result = text.textContent.includes("50.00%");
+        expect(result).toBe(true);
+    });
+
+    it('Test exam percentage off mock data executed in Correct then All order', async () => {
+        getCorrectListAdapter.emit(mockGetCorrectExamResultPoolQuestions);
+        getAllListAdapter.emit(mockGetAllExamResultPoolQuestions);
+        await flushPromises();
+        let text = element.shadowRoot.querySelector(".wrapper");
+        let result = text.textContent.includes("50.00%");
+        expect(result).toBe(true);
+    });
+
+    it('Test Page when both @wire returns errors', async () => {
+        getAllListAdapter.error();
+        getCorrectListAdapter.error();
+        await flushPromises();
+        let text = element.shadowRoot.querySelector("p");
+        let result = text.textContent.includes("There are no exam results related to the exam's pool");
+        expect(result).toBe(true);
+    });
 
 });
