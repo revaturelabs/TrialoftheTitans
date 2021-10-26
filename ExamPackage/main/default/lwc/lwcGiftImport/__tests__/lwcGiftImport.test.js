0e7af59027972864f8205c32c86aa871383ec206
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-10-26 13:59:49 -0400

    test update

--- ExamPackage/main/default/lwc/lwcGiftImport/__tests__/lwcGiftImport.test.js
+++ ExamPackage/main/default/lwc/lwcGiftImport/__tests__/lwcGiftImport.test.js
@@ -14,11 +14,21 @@ describe('c-lwc-gift-import', () => {
         const element = createElement('c-lwc-gift-import', {
             is: LwcGiftImport
         });
-        document.body.appendChild(component);
+        document.body.appendChild(element);
     });
 
     it('displays header', () => {
         const headerTag = element.shadowRoot.querySelector('h1');
         expect(headerTag.textContent).toBe('Import GIFT-Formatted Questions');
+    });
+
+    it('renders count of imported and updated questions', () => {
+        element.imported = 10;
+        element.updated = 5;
+
+        return Promise.resolve().then(() => {
+            const iTag = element.shadowRoot.querySelector('i');
+            expect(iTag.textContent).toEqual('Imported 10 questions - Updated 5 questions');
+        });
     });
 })
