a2e9b777171ec440c5b667ef63982504fa216a6a
Wayne He <70216125+wayneh000@users.noreply.github.com>
2021-10-26 14:56:48 -0400

    Changed scoping of element.

--- ExamPackage/main/default/lwc/lwcGiftImport/__tests__/lwcGiftImport.test.js
+++ ExamPackage/main/default/lwc/lwcGiftImport/__tests__/lwcGiftImport.test.js
@@ -1,34 +1,38 @@
-import { createElement } from 'lwc';
-import LwcGiftImport from '../lwcGiftImport';
+import { createElement } from "lwc";
+import LwcGiftImport from "../lwcGiftImport";
 
-describe('c-lwc-gift-import', () => {
-    // after each test, reset the DOM
-    afterEach(() => {
-        while (document.body.firstChild) {
-            document.body.removeChild(document.body.firstChild);
-        }
+describe("c-lwc-gift-import", () => {
+  let element;
+
+  // after each test, reset the DOM
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  // before each test, create component element and append to the DOM
+  beforeEach(() => {
+    element = createElement("c-lwc-gift-import", {
+      is: LwcGiftImport
     });
+    document.body.appendChild(element);
+  });
 
-    // before each test, create component element and append to the DOM
-    beforeEach(() => {
-        const element = createElement('c-lwc-gift-import', {
-            is: LwcGiftImport
-        });
-        document.body.appendChild(element);
+  it("displays header", () => {
+    const headerTag = element.shadowRoot.querySelector("h1");
+    expect(headerTag.textContent).toBe("Import GIFT-Formatted Questions");
+  });
+
+  it("renders count of imported and updated questions", () => {
+    element.imported = 10;
+    element.updated = 5;
+
+    return Promise.resolve().then(() => {
+      const iTag = element.shadowRoot.querySelector("i");
+      expect(iTag.textContent).toEqual(
+        "Imported 10 questions - Updated 5 questions"
+      );
     });
-
-    it('displays header', () => {
-        const headerTag = element.shadowRoot.querySelector('h1');
-        expect(headerTag.textContent).toBe('Import GIFT-Formatted Questions');
-    });
-
-    it('renders count of imported and updated questions', () => {
-        element.imported = 10;
-        element.updated = 5;
-
-        return Promise.resolve().then(() => {
-            const iTag = element.shadowRoot.querySelector('i');
-            expect(iTag.textContent).toEqual('Imported 10 questions - Updated 5 questions');
-        });
-    });
-})
+  });
+});
