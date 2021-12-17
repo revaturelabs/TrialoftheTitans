138bbb62b1620780203e8de962d78fe32a4924a7
EternityDoom <kale@dfddgame.com>
2021-12-17 10:32:03 -0700

    Created QuestionFlag LWC layout

--- ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
+++ ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
@@ -1,3 +1,26 @@
 import { LightningElement } from 'lwc';
 
-export default class QuestionFlag extends LightningElement {}
+export default class QuestionFlag extends LightningElement {
+    awaitState = true;
+    flagState = false;
+    noteState = false;
+    submittedState = false;
+    flagReason;
+    reviewReasons = [];
+
+    flagButtonClicked(){
+
+    }
+
+    unflagButtonClicked(){
+
+    }
+
+    flagReasonChanged(){
+
+    }
+
+    submitButtonClicked(){
+        
+    }
+}
