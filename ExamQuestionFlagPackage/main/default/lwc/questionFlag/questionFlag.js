0667e5053c921f2ac3b4cb54dbc9ad19b9f1abcc
EternityDoom <kale@dfddgame.com>
2021-12-22 08:31:32 -0700

    Fixed QuestionFlag syntax errors, made a few more profile fixes.

--- ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
+++ ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
@@ -7,6 +7,7 @@ export default class QuestionFlag extends LightningElement {
     submittedState = false;
     flagReason;
     reviewReasons = [];
+    reviewSubReasons = [];
 
     flagButtonClicked(){
 
