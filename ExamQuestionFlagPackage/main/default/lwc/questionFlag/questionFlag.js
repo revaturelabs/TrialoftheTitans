4b15c4f2cdafcc3ddd74834e604e5c148c960853
EternityDoom <kale@dfddgame.com>
2021-12-22 12:04:15 -0700

    Improvements to the QuestionFlag component and some profile fixes.

--- ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
+++ ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
@@ -1,23 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class QuestionFlag extends LightningElement {
-    awaitState = true;
-    flagState = false;
+    @api recordId;
+    specifiedId;
     noteState = false;
     submittedState = false;
-    flagReason;
     reviewReasons = [];
     reviewSubReasons = [];
 
-    flagButtonClicked(){
+    get questionId() {
+        if (this.specifiedId){
+            return this.specifiedId;
+        } else if (this.recordId){
+            return this.recordId;
+        }
+    }
 
+    @api set questionId(value){
+        this.specifiedId = value;
+        this.noteState = false;
+        this.submittedState = false;
+    }
+
+    get awaitState(){
+        return this.questionId == false;
+    }
+    
+    get flagState() {
+        return this.questionId == true && !this.noteState && !this.submittedState;
+    }
+
+    flagButtonClicked(){
+        this.noteState = true;
     }
 
     unflagButtonClicked(){
-
+        this.noteState = false;
     }
 
-    flagReasonChanged(){
+    reviewReasonChanged(){
 
     }
 
