e438f5c54e858fcc565df2bc49d20b744a54b679
Kaya66 <zekezimmermanit@gmail.com>
2021-12-22 15:39:18 -0500

    Uploading LWC and changes for teammate to work on.

--- HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
+++ HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getSkillList from '@salesforce/apex/lwcPortFolioIndustryEquivs.getSkillList';
 
-export default class LwcPortfolioIndustryEquivs extends LightningElement {}
+
+
+
+
+export default class LwcPortfolioIndustryEquivs extends LightningElement {
+isEdit = false;
+
+data = [];  //empty array to hold data from skill list
+@wire(getSkillList) skillList;
+populateData({error, data}){
+    if(data){
+        this.data = data;
+    }
+}
+
+
+
+
+}
+
+//skillList
