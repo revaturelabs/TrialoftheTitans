0dd26a82a5d30893881ef6a390cf5af9543d2d4b
Kaya66 <zekezimmermanit@gmail.com>
2021-12-23 15:18:05 -0500

    LWC is non functional.   Controller not passing data

--- HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
+++ HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
@@ -1,28 +1,33 @@
 import { LightningElement, api, wire } from 'lwc';
-import getSkillList from '@salesforce/apex/lwcPortFolioIndustryEquivs.getSkillList';
 
+import Return_Names from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnSkillNames';
+import GET_MONTHS from '@salesforce/apex/PortfolioIndustryEquivsController.ReturnMonths';
 
-
-
+import Id from '@salesforce/user/Id';
 
 export default class LwcPortfolioIndustryEquivs extends LightningElement {
 isEdit = false;
+userId = Id;
+//get current user id.  may need to pass it to the apex controller? 
 
-data = [];  //empty array to hold data from skill list
-@wire(getSkillList) skillList;
-populateData({error, data}){
-    if(data){
-        this.data = data;
-    }
-}
+
+
+NameData = [];  //empty array to hold data from skill list
+
+ @wire(Return_Names) NameList;
+ populateData({error, data}){
+     if(data){
+         this.data = NameData;
+     }
+ }
+
+
+
+
 
 edit(event){
     this.isEdit = !this.isEdit;
 }
 
-
-
-
 }
 
-//skillList
