1e8e1014b01ea45b7d4b3d9a28f1923c2525f586
Kaya66 <zekezimmermanit@gmail.com>
2021-12-28 16:42:53 -0500

    Record Display is operational

--- HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
+++ HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
@@ -1,33 +1,24 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 
-import Return_Names from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnSkillNames';
-import GET_MONTHS from '@salesforce/apex/PortfolioIndustryEquivsController.ReturnMonths';
-
-import Id from '@salesforce/user/Id';
+import RETURN_NAMES from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnSkillNames';
+import Id from '@salesforce/user/Id'; //check User Id on page for debug
 
 export default class LwcPortfolioIndustryEquivs extends LightningElement {
 isEdit = false;
 userId = Id;
-//get current user id.  may need to pass it to the apex controller? 
-
-
-
-NameData = [];  //empty array to hold data from skill list
-
- @wire(Return_Names) NameList;
- populateData({error, data}){
-     if(data){
-         this.data = NameData;
-     }
- }
 
 
 
 
 
+//get skill names and skill months
+@wire(RETURN_NAMES) NameList;
+
+
+
+ //swap between view and edit mode
 edit(event){
     this.isEdit = !this.isEdit;
 }
-
 }
 
