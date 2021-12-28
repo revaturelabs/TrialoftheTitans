aa2632b5d192476f57e1e78b276be01f47e37d33
Kaya66 <zekezimmermanit@gmail.com>
2021-12-28 17:41:10 -0500

    default data generator in apex and prep work for record edit in html file.

--- HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
+++ HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
@@ -1,19 +1,12 @@
 import { LightningElement, wire, api } from 'lwc';
 
 import RETURN_NAMES from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnSkillNames';
-import Id from '@salesforce/user/Id'; //check User Id on page for debug
 
 export default class LwcPortfolioIndustryEquivs extends LightningElement {
 isEdit = false;
-userId = Id;
-
-
-
-
 
 //get skill names and skill months
 @wire(RETURN_NAMES) NameList;
-
 
 
  //swap between view and edit mode
