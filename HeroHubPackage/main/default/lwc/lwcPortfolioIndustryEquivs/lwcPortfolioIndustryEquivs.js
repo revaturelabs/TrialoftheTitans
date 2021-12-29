36b6dca87d339a1a2fe0aa1ccfae1ceb2b35c87a
Kaya66 <zekezimmermanit@gmail.com>
2021-12-29 16:16:21 -0500

    Loading edit records is functional.  saving edits throws error.
    for LWC.html    added for:each template directive for record edits.
    
    for apex    added method to get 5 record id's from the active user.

--- HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
+++ HeroHubPackage/main/default/lwc/lwcPortfolioIndustryEquivs/lwcPortfolioIndustryEquivs.js
@@ -1,17 +1,38 @@
 import { LightningElement, wire, api } from 'lwc';
 
 import RETURN_NAMES from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnSkillNames';
+import APEX_ID from '@salesforce/apex/lwcPortFolioIndustryEquivs.returnId';
+import NAME_FIELD from '@salesforce/schema/Equivalency__c.Name';
+import EQUIV_FIELD from '@salesforce/schema/Equivalency__c.Skill_Equivalency__c';
+import EQUIV_ID from '@salesforce/schema/Equivalency__c.id';
+import EQUIV_OBJ from '@salesforce/schema/Equivalency__c';
 
 export default class LwcPortfolioIndustryEquivs extends LightningElement {
-isEdit = false;
+    @api recordId
+    @api objectApiName
+    
+    isEdit = false;
+    recordId = EQUIV_ID;
+    objectApiName = EQUIV_OBJ;
+    name = NAME_FIELD;
+    equiv = EQUIV_FIELD;
+    
+    //get skill names and skill months
+    @wire(RETURN_NAMES) NameList;
+    @wire(APEX_ID) recordIdList;
 
-//get skill names and skill months
-@wire(RETURN_NAMES) NameList;
+    //swap between view and edit mode
+    edit(event){
+        this.isEdit = !this.isEdit;
+    }
 
-
- //swap between view and edit mode
-edit(event){
-    this.isEdit = !this.isEdit;
-}
+    handleSuccess(){
+        const toastEvent = new ShowToastEvent({
+            title: "Saved",
+            message: "Record Saved",
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
 }
 
