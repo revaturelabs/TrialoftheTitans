59786d1a59ed0eae2236de7db483be3f58596933
EternityDoom <kale@dfddgame.com>
2022-01-03 08:25:42 -0700

    Updates for LWC Javascript
    Not yet complete, not yet tested, but revised to handle state checking and errors better.

--- ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
+++ ExamQuestionFlagPackage/main/default/lwc/questionFlag/questionFlag.js
@@ -5,40 +5,52 @@ import flagQuestion from '@salesforce/apex/QuestionFlagUpdates.flagQuestion';
 
 export default class QuestionFlag extends LightningElement {
     @api recordId;
-    specifiedId;
-    noteState = false;
-    submittedState = false;
+    flagInProgress = false;
+    submitted = false;
+    errorMessage;
     reviewReasons = [];
     reviewSubReasons = [];
 
-    get questionId() {
-        if (this.specifiedId){
-            return this.specifiedId;
-        } else if (this.recordId){
-            return this.recordId;
-        }
-    }
-
-    @api set questionId(value){
-        this.specifiedId = value;
-        this.noteState = false;
-        this.submittedState = false;
-    }
-
     get awaitState(){
-        return this.questionId == false;
+        return this.recordId == false;
     }
     
     get flagState() {
-        return this.questionId == true && !this.noteState && !this.submittedState;
+        return this.recordId == true && this.flagInProgress == false && this.submitted == false;
+    }
+
+    get noteState(){
+        return this.recordId == true && this.flagInProgress == true;
+    }
+
+    get submittedState(){
+        return this.recordId == true && this.flagInProgress == false && this.submitted == true;
     }
 
     flagButtonClicked(){
-        this.noteState = true;
+        this.flagInProgress = true;
+        this.retrieveQuestionFlagState();
+    }
+
+    async retrieveQuestionFlagState() {
+        const categoryDropdown = this.template.querySelector('[data-name="category"]');
+        const subcategoryDropdown = this.template.querySelector('[data-name="subcategory"]');
+        const notesTextbox = this.template.querySelector('[data-name="flagNotesInput"]');
+        let flagState = await lookupQuestion({questionId: this.recordId});
+        if (flagState){
+            if (flagState[0]){
+                categoryDropdown.value = flagState[0];
+                subcategoryDropdown.value = flagState[1];
+                notesTextbox.value = flagState[2];
+            }
+        } else {
+            this.errorMessage = 'Unable to look up question. Id ' + this.recordId + ' not found.';
+            this.recordId = null;
+        }
     }
 
     unflagButtonClicked(){
-        this.noteState = false;
+        this.flagInProgress = false;
     }
 
     reviewReasonChanged(){
