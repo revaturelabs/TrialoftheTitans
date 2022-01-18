b2410d1aa614a31eacd5c2fe1fe404e5f75399eb
Andrew Emond <andyemond123@gmail.com>
2022-01-18 10:22:46 -0500

    adding sidebar to github

---
+++ HeroHubPackage/main/default/lwc/lwc/sideBar/sideBar.js
@@ -0,0 +1,37 @@
+import getAccount from '@salesforce/apex/sideBarHelper.getAccount';
+import getCert from '@salesforce/apex/sideBarHelper.getCert';
+import getSquad from '@salesforce/apex/sideBarHelper.getSquad';
+import { LightningElement,wire } from 'lwc';
+
+export default class SideBar extends LightningElement {
+    @wire(getAccount) accInfo;
+    @wire(getSquad) squadInfo;
+    @wire(getCert) certInfo;
+    clickedButtonLabel;
+    squadName;
+    heroName;
+    heroTitle;
+    heroArete;
+    certName;
+    certDate;
+    certAssessor;
+    certVer;
+    placeholderChart = "https://lh3.googleusercontent.com/g0Jw-I6-gH2DVCpnl3u8QKZVT_meR9lcJlpyeSZ-MyvwLnyEZvgyrY5frldA8HCv55s=w280-rwa"
+    portName;
+    portStatus;
+    renderedCallback(){
+        if(this.accInfo.data && this.squadInfo.data && this.certInfo.data){
+            console.log(this.squadInfo)
+            this.squadName = "Our Hero's Squad: " +this.squadInfo.data[0].Name
+            this.heroName = "Our Hero's Name: " + this.accInfo.data[0].Name
+            this.heroArete = "Our Hero's Arete: "
+            this.heroTitle = "Our Hero's Title: "
+            this.certName = "Certification Name: "+this.certInfo.data[0].Name
+            this.certDate = "Certification Date: " +this.certInfo.data[0].Date_Issued__c
+            this.certAssessor = "Certification Assessor: "+this.certInfo.data[0].Assessor__c
+            this.certVer = "Certification Verifyer: " +this.certInfo.data[0].Verification_Site__c
+            this.portName = "Portfolio Name :"
+            this.portStatus = "Portfolio Status :"
+        }
+    }
+}
