189e62f18d34abe0504c1acbd551a5026452849d
Trevor Colton <trevor.colton@revature.net>
2022-01-18 16:06:41 -0700

    Updated controller

--- HeroHubPackage/main/default/lwc/titanDisplayBar/titanDisplayBar.js
+++ HeroHubPackage/main/default/lwc/titanDisplayBar/titanDisplayBar.js
@@ -1,11 +1,20 @@
 import { LightningElement, track, api } from "lwc";
 
+import getCurrentUser from "@salesforce/apex/titanDisplayController.getCurrentUser";
 export default class TitanDisplayBar extends LightningElement {
     @track disableOverview = false;
     @track disableAdvance = false;
     @api name;
+    @api titanId;
 
-    connectedCallback() {}
+
+
+    connectedCallback() {
+        let user = getCurrentUser();
+        user.then(res => {
+            console.log('Result: ' + res);
+        })
+    }
 
     handleOverview() {}
 
