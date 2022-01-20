030986e77d2cf17e1004eb0880dd9cb54de78bf9
harveyhill <43756739+harveyhill@users.noreply.github.com>
2022-01-20 16:34:55 -0500

    Revert "publish and subscribe created"
    This reverts commit ce4ac5fa01a447a7805bdddff01b5599270194b1.

--- TrialoftheTitansPackage/main/default/lwc/notifications/notifications.js
+++ TrialoftheTitansPackage/main/default/lwc/notifications/notifications.js
@@ -1,32 +1,11 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import notificationIcon from '@salesforce/resourceUrl/notificationBell_small';
-
-import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
-import DATALMS from '@salesforce/messageChannel/notification__c';
-
 export default class Notifications extends LightningElement {
     notification = notificationIcon;
     showDropdown = false;
 
-
-    @wire(MessageContext) 
-    messageContext;
-
     clickHandler(event){
         event.preventDefault();
         this.showDropdown = !this.showDropdown;
-    }
-
-
-    connectedCallback() {
-        subscribe(
-            this.messageContext,
-            DATALMS,
-            (message) => { this.handleData(message) }, {scope: APPLICATION_SCOPE}
-        );
-    }
-
-    handleData(message){
-        console.log(message);
     }
 }
