ce4ac5fa01a447a7805bdddff01b5599270194b1
Deep Patel <patel.deep1217@outlook.com>
2022-01-20 14:54:37 -0500

    publish and subscribe created

--- TrialoftheTitansPackage/main/default/lwc/notifications/notifications.js
+++ TrialoftheTitansPackage/main/default/lwc/notifications/notifications.js
@@ -1,11 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import notificationIcon from '@salesforce/resourceUrl/notificationBell_small';
+
+import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
+import DATALMS from '@salesforce/messageChannel/notification__c';
+
 export default class Notifications extends LightningElement {
     notification = notificationIcon;
     showDropdown = false;
 
+
+    @wire(MessageContext) 
+    messageContext;
+
     clickHandler(event){
         event.preventDefault();
         this.showDropdown = !this.showDropdown;
+    }
+
+
+    connectedCallback() {
+        subscribe(
+            this.messageContext,
+            DATALMS,
+            (message) => { this.handleData(message) }, {scope: APPLICATION_SCOPE}
+        );
+    }
+
+    handleData(message){
+        console.log(message);
     }
 }
