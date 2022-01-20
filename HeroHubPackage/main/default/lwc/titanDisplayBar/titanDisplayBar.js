a3d1114f232820f43364c420c3059a67f96b31d5
Matthew Lewandowski <mblewandowski96@gmail.com>
2022-01-19 19:09:27 -0500

    fixed up the titanView

--- HeroHubPackage/main/default/lwc/titanDisplayBar/titanDisplayBar.js
+++ HeroHubPackage/main/default/lwc/titanDisplayBar/titanDisplayBar.js
@@ -1,11 +1,18 @@
 import { LightningElement, track, api } from "lwc";
+import getTitanById from "@salesforce/apex/titanDisplayController.getTitanById";
 
 export default class TitanDisplayBar extends LightningElement {
     @track disableOverview = false;
     @track disableAdvance = false;
-    @api name;
+    @api id;
+    @track titanName;
 
-    connectedCallback() {}
+    renderedCallback() {
+        let titan = getTitanById({ identifier: this.id.slice(0, -3) });
+        titan.then((res) => {
+            this.titanName = res[0].Name;
+        });
+    }
 
     handleOverview() {}
 
