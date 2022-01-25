786d3be62ad486673b91562c9a6743a692459e21
Matthew Lewandowski <mblewandowski96@gmail.com>
2022-01-25 11:04:18 -0500

    lms

--- HeroHubPackage/main/default/lwc/updatedSquadLeaderboard/updatedSquadLeaderboard.js
+++ HeroHubPackage/main/default/lwc/updatedSquadLeaderboard/updatedSquadLeaderboard.js
@@ -1,33 +1,30 @@
-import {LightningElement, wire, track} from 'lwc';
-import getUpdatedScore from '@salesforce/apex/updatedSquadLeaderBoardHandler.getUpdatedScore';
+import { LightningElement, wire, track } from "lwc";
+import { publish, MessageContext, createMessageContext } from "lightning/messageService";
+import NOTIFICATION from "@salesforce/messageChannel/notification__c";
+import getUpdatedScore from "@salesforce/apex/updatedSquadLeaderBoardHandler.getUpdatedScore";
 
 const COLUMNS = [
-    {label: 'Hero Name', fieldName: 'Name', hideDefaultActions:true, type: 'text'},
-    {label: 'Hero Score', fieldName: 'Weekly_Arete_Number__c', hideDefaultActions:true, type: 'Number'}
-]
-export default class UpdatedSquadLeaderboard extends LightningElement 
-{
+    { label: "Hero Name", fieldName: "Name", hideDefaultActions: true, type: "text" },
+    { label: "Hero Score", fieldName: "Weekly_Arete_Number__c", hideDefaultActions: true, type: "Number" }
+];
+export default class UpdatedSquadLeaderboard extends LightningElement {
     @track squadName;
     @track accounts;
     @track columns = COLUMNS;
     @track error;
 
     @wire(getUpdatedScore)
-    wiredupdatedScore({ error, data }) 
-    {
-        if (data) 
-        {
+    wiredupdatedScore({ error, data }) {
+        if (data) {
             this.accounts = data;
-            this.error = undefined;
-            this.squadName = 'Squad Leaderboard for: ' + this.accounts[0].Squad__r.Name;
-            console.log(this.accounts);
-        } 
-        
-        else if (error) 
-        {
+            this.squadName = "Squad Leaderboard for: " + this.accounts[0].Squad__r.Name;
+            console.log(data);
+        } else if (error) {
             this.error = error;
-            this.accounts = undefined;
         }
     }
 
+    publishMessage(message) {
+        publish(this.context, NOTIFICATION, message);
+    }
 }
