70e7322ac4534065e1afd4fbe5f259978f531f6b
Trevor Colton <trevor.colton@revature.net>
2022-01-25 11:56:45 -0700

    Made Radar Chart Responsive

--- ChartPackage/main/default/staticresources/RadarChart/radarChart.js
+++ ChartPackage/main/default/staticresources/RadarChart/radarChart.js
@@ -54,6 +54,8 @@ window.RadarChart = function(id, data, options) {
 	var svg = d3.select(id).append("svg")
 			.attr("width",  cfg.w + cfg.margin.left + cfg.margin.right)
 			.attr("height", cfg.h + cfg.margin.top + cfg.margin.bottom)
+			.attr("viewBox", [0, 0, cfg.w + cfg.margin.left + cfg.margin.right, cfg.h + cfg.margin.top + cfg.margin.bottom])
+			.attr("preserveAspectRatio", "xMinYMin meet")
 			.attr("class", "radar"+id);
 	//Append a g element		
 	var g = svg.append("g")
