b6b7060435bee47cbe62e6af9448c7af86ffc84d
Nick de Sousa <93998652+ND14543@users.noreply.github.com>
2022-01-27 15:43:53 -0500

    Update dataFactory.js

--- DataFactory/lwc/dataFactory/dataFactory.js
+++ DataFactory/lwc/dataFactory/dataFactory.js
@@ -3,6 +3,8 @@ import GenerateData from '@salesforce/apex/ApexDataFactory.GenerateData';
 import deleteStuff from '@salesforce/apex/ApexDataFactory.deleteStuff';
 
 export default class DataFactory extends LightningElement {
+    //ShowDataButton determines whether the delete data button or the generate data button is displayed, 
+    //the user will always be prompted to delete before generation of data is allowed
     ShowDataButton = false;
     handleDeleteClick(){
         deleteStuff();
