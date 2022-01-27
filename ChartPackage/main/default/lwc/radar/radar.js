61b3d3d06060e645cbc628b33f97755cbc7716bc
Levvine <43613739+Levvine@users.noreply.github.com>
2022-01-27 15:54:00 -0500

    Updated documentation on radar.js

--- ChartPackage/main/default/lwc/radar/radar.js
+++ ChartPackage/main/default/lwc/radar/radar.js
@@ -40,14 +40,24 @@ export default class Radar extends LightningElement {
     }
 
     // initializes radar chart configurations and attaches it to div.
-    initializeD3() {
+    initializeD3() 
+        // margin: margins between outermost circle and bounding box, including text
+        // width: starting width of the entire radar chart, including margins
+        //        set to whichever is smaller b/t maxwidth and window width
+        // height: starting height of the entire chart, including margins
+        //        set to whichever is smaller b/t starting width and window height
         var margin = { top: 40, right: 40, bottom: 40, left: 40 };
         var width = Math.min(this.maxWidth, window.innerWidth - 10) - margin.left - margin.right;
         var height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
 
+        // colors of each radar, by index
+        // each additional radar beyond the index will be assigned the final color
         var color = d3.scale.ordinal()
             .range(["#EDC951", "#CC333F", "#00A0B0", "#7289ad"])
 
+        // maxValue: the maximum axis value
+        // levels: number of rings/intervals in the radar chart
+        // roundstrokes: if true, rounds out the corners and edges of a radar
         var radarChartOptions = {
             w: width,
             h: height,
@@ -58,7 +68,7 @@ export default class Radar extends LightningElement {
             color: color
         }
 
-        //Call function to draw the Radar chart
+        // Call function to draw the Radar chart
         RadarChart(this.template.querySelector('div.d3'), this.dat, radarChartOptions)
     }
 
