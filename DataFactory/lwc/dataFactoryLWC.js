da931733b80e97fa567400b3b8f61edfca353664
ashbeenow123 <94003927+ashbeenow123@users.noreply.github.com>
2022-01-28 06:58:51 -0800

    Create dataFactoryLWC.js
    javascript based on Chris Eng's whiteboarding

---
+++ DataFactory/lwc/dataFactoryLWC.js
@@ -0,0 +1,29 @@
+////javascript based on Chris Eng's whiteboarding posted on Discord on 01/19th and goes with the single button version///
+{ LightningElement,wire,track } from 'lwc';
+import CreateCurriculum from '@salesforce/apex/ApexDataFactory.CreateCurriculum';
+import CreateTitan from '@salesforce/apex/ApexDataFactory.CreateTitan';
+import CreateSkill from '@salesforce/apex/ApexDataFactory.CreateSkill';
+import CreateCohort from '@salesforce/apex/ApexDataFactory.CreateCohort';
+import CreateTeam from '@salesforce/apex/ApexDataFactory.CreateTeam';
+import CreateSquad from '@salesforce/apex/ApexDataFactory.CreateSquad';
+import CreateAccount from '@salesforce/apex/ApexDataFactory.CreateAccount';
+
+
+
+
+
+
+
+export default class LwcDataFactory extends LightningElement{
+    handleClick(){
+        CreateCurriculum();  
+        CreateCohort();
+        CreateTeam();
+        CreateSquad();
+        CreateAccount();
+        CreateTitan();
+        CreateSkill();
+        
+
+    }
+}
