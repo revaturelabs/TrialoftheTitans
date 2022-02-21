f6a100486884d01b4c9c1fd07494727e29c9df87
MoAdam10 <73980144+MoAdam10@users.noreply.github.com>
2022-02-21 16:02:18 -0500

    Add files via upload

--- TitanHubPackage/main/default/lwc/lwcPathToTitan/lwcPathToTitan.js
+++ TitanHubPackage/main/default/lwc/lwcPathToTitan/lwcPathToTitan.js
@@ -11,12 +11,12 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 import { LightningElement, api, wire } from 'lwc';
-
+import { NavigationMixin } from 'lightning/navigation';
 import getAccount from "@salesforce/apex/PathToTitanController.getAccount";
 import getSquad from "@salesforce/apex/PathToTitanController.getSquad";
 import getExamAndResultsList from "@salesforce/apex/PathToTitanController.getExamAndResultsList";
 
-export default class LwcPathToTitan extends LightningElement {
+export default class LwcPathToTitan extends NavigationMixin(LightningElement) {
 
     // Variables to Display Hero Details at Top of Component
     heroArete;
@@ -49,16 +49,17 @@ export default class LwcPathToTitan extends LightningElement {
     // Navigation Function to pass examId and accountId to lwcExamInterview
     handleExamClick(event) {
         let exam_data_attribute = event.target.getAttribute("data-exam");
-
+        console.log(exam_data_attribute);
+       
         this[NavigationMixin.Navigate]({
             type: "comm__namedPage",
             attributes: {
-                name: "Exam_Interview__c" //API name of the page to navigate to
+                name: "Exam_Interview__c"//API name of the page to navigate to
             },
              state: {
                 c__examId: exam_data_attribute,
-                c__accId: this.currentUser.Id
+                c__accId: this.accountId
              }
-        });      
+        });
     }
 }
