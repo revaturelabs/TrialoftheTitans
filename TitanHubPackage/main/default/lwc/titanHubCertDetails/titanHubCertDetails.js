77706798c4c66348c3e0fbda0936f273325ef76f
Phillip Nguyen-Phan <phillip.nguyenphan@revature.net>
2022-02-22 10:15:13 -0800

    attempted to fix certdetails

--- TitanHubPackage/main/default/lwc/titanHubCertDetails/titanHubCertDetails.js
+++ TitanHubPackage/main/default/lwc/titanHubCertDetails/titanHubCertDetails.js
@@ -4,9 +4,14 @@
 // Author: Alan Huang
 // Created: 01/25/2022
 // Updated: 01/25/2022
+// Updated - Iteration-X : 02/22/2022
 // Description: Certification details component for Hero Hub - Titan Overview
 // 
 /////////////////////////////////////////////////////////////////////////////////
+
+
+// Iteration-X could not get the certification values to load. The data correctly pulls from org but it is having errors displaying them
+// No documentation was found at the time to figure how to set it up correctly.
 
 import { LightningElement, wire, api } from 'lwc';
 import getCertDetails from '@salesforce/apex/titanHubCertDetailsController.getCertDetails';
@@ -22,14 +27,11 @@ export default class TitanHubCertDetails extends LightningElement {
             this.certDetails = data;
             this.error = undefined;
             console.log(this.certDetails);
-            console.error(error);
         } else if (error) {
             this.certDetails = undefined;
             this.error = error;
             console.error(error);
         }
     };
-    
-    
 
 }
