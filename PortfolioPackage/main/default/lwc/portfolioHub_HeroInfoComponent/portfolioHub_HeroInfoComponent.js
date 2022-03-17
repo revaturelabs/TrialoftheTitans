bc3dc773721ff820d30071b4da0cb57f5b430916
sean-rw <seanrwhitley@gmail.com>
2022-03-16 21:42:45 -0700

    Prepping for data

--- PortfolioPackage/main/default/lwc/portfolioHub_HeroInfoComponent/portfolioHub_HeroInfoComponent.js
+++ PortfolioPackage/main/default/lwc/portfolioHub_HeroInfoComponent/portfolioHub_HeroInfoComponent.js
@@ -1,3 +1,9 @@
+/** 
+ * Created by Sean Whitley 3/16/22
+ * cookie-crisp#3847
+ * sean.whitley@revature.net
+ */
+
 import { LightningElement, wire } from 'lwc';
 
 export default class PortfolioHub_HeroInfoComponent extends LightningElement {
@@ -19,6 +25,7 @@ export default class PortfolioHub_HeroInfoComponent extends LightningElement {
      *      (portfolio approval status)
      */
     // TODO: Define Apex method for data retrieval
+    //      [SELECT User__r.MediumPhotoUrl,User__r.Name,Name FROM Job__c]
     // @wire(getHeroInfo)
     heroInfo({ error, data }) {
         if (error) {
@@ -27,8 +34,8 @@ export default class PortfolioHub_HeroInfoComponent extends LightningElement {
         }
 
         // If there's no error, data will be returned
-        this.profileImgSrc  = data.profileImgSrc;
-        this.heroName       = data.heroName;
+        this.profileImgSrc  = data.MediumPhotoUrl;
+        this.heroName       = data.Name;
         this.heroTitle      = data.heroTitle;
         this.approvalStatus = data.approvalStatus;
     }
