ca6d5898c699d50ab54f9225e7ba5698211f6e28
Mohammed Azad <61253901+chuckleberrynip@users.noreply.github.com>
2022-03-23 15:09:13 -0400

    Add files via upload

--- PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
+++ PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
@@ -1,4 +1,4 @@
-import { LightningElement, track, wire, api } from 'lwc';
+import { LightningElement } from 'lwc';
 import CARDBONEDUCATION from '@salesforce/resourceUrl/carboneducation';
 export default class Portfolioeducationcompartment extends LightningElement {
     carboneducation = CARDBONEDUCATION; 
