241ac1be207e4e165da30a9e79019aaa2f855f7f
Mohammed Azad <61253901+chuckleberrynip@users.noreply.github.com>
2022-03-23 13:23:28 -0400

    Now individual entries are made

--- PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
+++ PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import CARDBONEDUCATION from '@salesforce/resourceUrl/carboneducation';
 export default class Portfolioeducationcompartment extends LightningElement {
     carboneducation = CARDBONEDUCATION; 
