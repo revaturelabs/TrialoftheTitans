f1e344c96f8e70076c98f41a80d7e36aa317b7de
Mountain Scott <mountainscott@yahoo.com>
2022-03-23 11:19:09 -0700

    added lightning record form [untested] [mountain]

--- PortfolioPackage/main/default/lwc/strengths/strengths.js
+++ PortfolioPackage/main/default/lwc/strengths/strengths.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class Strengths extends LightningElement {}
+export default class Strengths extends LightningElement {
+        //create a property
+    // @track strengths;
+    // //create a method
+    // @wire(getStrengths)
+    // wiredStrengths({ error, data }) {
+    //     if (data) {
+    //         this.strengths = data;
+    //     } else if (error) {
+    //         console.log(error);
+    //     }
+    // }
+    
+    showEditStrengthsBoolean = false;
+
+    //create a method
+    showEditStrengthsForm() {
+        console.log('showEditStrengthsForm');
+        this.showEditStrengthsBoolean = true;
+        //create an event
+        const showEditStrengthsEvent = new CustomEvent('showeditstrengthsform');
+        //dispatch the event
+        this.dispatchEvent(showEditStrengthsEvent);
+    }
+}
