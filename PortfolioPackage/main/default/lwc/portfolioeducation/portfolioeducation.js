adf099e5c6c7ba7bbfa9fa2af91a5873e9d33b6a
Mohammed Azad <61253901+chuckleberrynip@users.noreply.github.com>
2022-03-23 15:35:20 -0400

    Created a modal pop

--- PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
+++ PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
@@ -1,10 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import HATICON from '@salesforce/resourceUrl/hat';
 import EDITICON from '@salesforce/resourceUrl/editicon';
 export default class Portfolioeducation extends LightningElement {
-   hatty = HATICON;
-   editiconimplementation = EDITICON;
-   handleEvent(){ 
-       alert("Something");
-   }
+
+    hatty = HATICON;
+    @track modalChecker = false;
+    editiconimplementation = EDITICON;
+    modalOpener() 
+    {
+        this.modalChecker = true;
+    }
+    modalCloser() 
+    {
+        this.modalChecker = false;
+    }
 }
