5d27f5776ea7a8c2113406cd33f81675bca9f2fc
Mohammed Azad <mojangles32@gmail.com>
2022-03-24 15:05:32 -0400

    functional

--- PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
+++ PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
@@ -1,5 +1,19 @@
 import { LightningElement, track, wire, api } from 'lwc';
+import RETURN_EDUCATION from '@salesforce/apex/GetEducationInformation.returnEducationList';
+import { refreshApex } from '@salesforce/apex';
 //import CARDBONEDUCATION from '@salesforce/resourceUrl/carboneducation';
 export default class Portfolioeducationcompartment extends LightningElement {
     //carboneducation = CARDBONEDUCATION; 
+
+    @track education;
+    @track wireValue;
+
+    @wire(RETURN_EDUCATION)
+    educationList(value) {
+        const {error, data} = value;
+        this.education = data;
+        this.wireValue = value;
+    }
+    
+    
 }
