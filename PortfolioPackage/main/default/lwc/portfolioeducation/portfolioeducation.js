ee07a3b6bac9f0897c9682896b342e2304c084af
Mohammed Azad <mojangles32@gmail.com>
2022-03-24 11:28:04 -0400

    added record form

--- PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
+++ PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
@@ -1,4 +1,10 @@
 import { LightningElement, track, wire, api } from 'lwc';
+import EDUCATION_OBJECT from '@salesforce/schema/Education__c';
+import USER_FIELD from '@salesforce/schema/Education__c.User__c';
+import GPA_FIELD from '@salesforce/schema/Education__c.Gpa__C';
+import MAJOR_FIELD from '@salesforce/schema/Education__c.Major__c';
+import DATE_GRADUATED from '@salesforce/schema/Education__c.DateGraduate__c';
+import DEGREE_FIELD from '@salesforce/schema/Education__c.Degree__c';
 //import HATICON from '@salesforce/resourceUrl/hat';
 //import EDITICON from '@salesforce/resourceUrl/editicon';
 export default class Portfolioeducation extends LightningElement {
@@ -6,7 +12,16 @@ export default class Portfolioeducation extends LightningElement {
     hatty = HATICON;
     editiconimplementation = EDITICON;
 */    
+    @api recordId;
+    @api objectApiName;
     @track modalChecker = false;
+    educationObject = EDUCATION_OBJECT;
+    userField = USER_FIELD;
+    majorField = MAJOR_FIELD;
+    degreeField = DEGREE_FIELD;
+    gpaField = GPA_FIELD;
+    dateGraduateField = DATE_GRADUATED;
+
     
     modalOpener() 
     {
