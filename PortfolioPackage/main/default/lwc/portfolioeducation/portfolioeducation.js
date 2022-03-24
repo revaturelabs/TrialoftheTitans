4207b0c18f25876ee0703514d1f246f65a480465
Mohammed Azad <mojangles32@gmail.com>
2022-03-24 13:56:09 -0400

    fixed lightning modal form

--- PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
+++ PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
@@ -4,7 +4,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import EDUCATION_OBJECT from '@salesforce/schema/Education__c';
 import USER_FIELD from '@salesforce/schema/Education__c.User__c';
 import EDUCATION_FIELD from '@salesforce/schema/Education__c.Name';
-import GPA_FIELD from '@salesforce/schema/Education__c.Gpa__C';
+import GPA_FIELD from '@salesforce/schema/Education__c.Gpa__c';
 import MAJOR_FIELD from '@salesforce/schema/Education__c.Major__c';
 import DATE_GRADUATED from '@salesforce/schema/Education__c.DateGraduate__c';
 import DEGREE_FIELD from '@salesforce/schema/Education__c.Degree__c';
@@ -26,7 +26,7 @@ export default class Portfolioeducation extends LightningElement {
     majorField = MAJOR_FIELD;
     degreeField = DEGREE_FIELD;
     gpaField = GPA_FIELD;
-    dateGraduateField = DATE_GRADUATED;
+    dateGraduatedField = DATE_GRADUATED;
 
     
     modalOpener() 
