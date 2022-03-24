627405e2dea765165aa1ae8221109b74ef6be56e
Mountain Scott <mountainscott@yahoo.com>
2022-03-24 08:40:07 -0700

    progressbar now connected to orgdata with a lot of help from DAVID [Mountain]

--- PortfolioPackage/main/default/lwc/strengths/strengths.js
+++ PortfolioPackage/main/default/lwc/strengths/strengths.js
@@ -1,17 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getEquivalencies from '@salesforce/apex/GetEquivalencies.getEq';
 
 export default class Strengths extends LightningElement {
         //create a property
-    // @track strengths;
+    @track strengths;
+
+//     0: {Name: 'Apex', Skill_Equivalency__c: 100, Id: 'a005c00001ke1k4AAA'}
+// 1: {Name: 'Process Automation', Skill_Equivalency__c: 94, Id: 'a005c00001ke1kTAAQ'}
+// 2: {Name: 'SQL', Skill_Equivalency__c: 83, Id: 'a005c00001ke1kPAAQ'}
+// 3: {Name: 'JavaScript', Skill_Equivalency__c: 78, Id: 'a005c00001ke1kOAAQ'}
+// 4: {Name: 'Java', Skill_Equivalency__c: 76, Id: 'a005c00001ke1kdAAA'}
     // //create a method
-    // @wire(getStrengths)
-    // wiredStrengths({ error, data }) {
-    //     if (data) {
-    //         this.strengths = data;
-    //     } else if (error) {
-    //         console.log(error);
-    //     }
-    // }
+    @wire(getEquivalencies)
+        getStrengths({ error, data }) {
+            if (data) {
+                this.strengths= data;
+                console.log('getStrengths', '********');
+                console.log(this.strengths);
+            } else if (error) {
+                console.log(error);
+            }
+    }
     
     showEditStrengthsBoolean = false;
 
