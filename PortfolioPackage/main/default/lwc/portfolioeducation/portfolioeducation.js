de8e4b03cc239fdd7df71909ab10bcc050dc3c05
Mohammed Azad <61253901+chuckleberrynip@users.noreply.github.com>
2022-03-24 10:06:27 -0400

    Change to Salesforce Icons

--- PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
+++ PortfolioPackage/main/default/lwc/portfolioeducation/portfolioeducation.js
@@ -1,11 +1,13 @@
 import { LightningElement, track, wire, api } from 'lwc';
-import HATICON from '@salesforce/resourceUrl/hat';
-import EDITICON from '@salesforce/resourceUrl/editicon';
+//import HATICON from '@salesforce/resourceUrl/hat';
+//import EDITICON from '@salesforce/resourceUrl/editicon';
 export default class Portfolioeducation extends LightningElement {
-
+/*
     hatty = HATICON;
-    @track modalChecker = false;
     editiconimplementation = EDITICON;
+*/    
+    @track modalChecker = false;
+    
     modalOpener() 
     {
         this.modalChecker = true;
--- PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
+++ PortfolioPackage/main/default/lwc/portfolioeducationcompartment/portfolioeducationcompartment.js
@@ -1,5 +1,5 @@
 import { LightningElement, track, wire, api } from 'lwc';
-import CARDBONEDUCATION from '@salesforce/resourceUrl/carboneducation';
+//import CARDBONEDUCATION from '@salesforce/resourceUrl/carboneducation';
 export default class Portfolioeducationcompartment extends LightningElement {
-    carboneducation = CARDBONEDUCATION; 
+    //carboneducation = CARDBONEDUCATION; 
 }
