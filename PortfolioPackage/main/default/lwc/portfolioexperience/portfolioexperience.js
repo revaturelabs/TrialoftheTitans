bc9139d06bbdc120b8b6dd291fd2176f05f4e1d5
Mohammed Azad <mojangles32@gmail.com>
2022-03-25 11:43:26 -0400

    Added employment component

---
+++ PortfolioPackage/main/default/lwc/portfolioexperience/portfolioexperience.js
@@ -0,0 +1,70 @@
+/************************************************************
+ * Done by: Mohammed Azad
+ * Create a list of a person's employment history.
+ * Date: March 24 2022
+ ************************************************************/
+
+import { LightningElement, track, wire, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import EXPERIENCE_OBJECT from '@salesforce/schema/Experience__c';
+import COMPANY_FIELD from '@salesforce/schema/Experience__c.Company__c';
+import POSITION_FIELD from '@salesforce/schema/Experience__c.Position__c';
+import START_DATE_FIELD from '@salesforce/schema/Experience__c.Start_Date__c';
+import END_DATE_FIELD from '@salesforce/schema/Experience__c.End_Date__c';
+
+import RETURN_EXPERIENCE from '@salesforce/apex/GetExperienceInformation.returnExperienceList';
+export default class Portfolioexperience extends LightningElement
+{
+    @api recordId;
+    @api objectApiName;
+
+    @track modalChecker = false;
+
+    experienceObject = EXPERIENCE_OBJECT;
+    companyField = COMPANY_FIELD;
+    positionField = POSITION_FIELD;
+    startDateField = START_DATE_FIELD;
+    endDateField = END_DATE_FIELD;
+
+
+    modalOpener() 
+    {
+        this.modalChecker = true;
+    }
+    modalCloser() 
+    {
+        const closeenv = new ShowToastEvent({
+            title: "Canceled",
+            message: "You canceled inputting information.",
+            variant: "error"
+        });
+
+        this.dispatchEvent(closeenv);
+        this.modalChecker = false;
+    }
+
+    handleSuccess() 
+    {
+        const env = new ShowToastEvent({
+            title: "Success",
+            message: "Information Received.",
+            variant: "success"
+        });
+
+        this.dispatchEvent(env);
+        this.modalChecker = false;
+    }
+
+    @track experience;
+    @track wireValue;
+
+    @wire(RETURN_EXPERIENCE)
+    experienceList(value) 
+    {
+        const {error, data} = value;
+        this.experience = data;
+        this.wireValue = value;
+    }
+    
+}
