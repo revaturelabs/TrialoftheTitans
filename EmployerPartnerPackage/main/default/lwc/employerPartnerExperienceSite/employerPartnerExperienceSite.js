4283f863a140f83646558c5ddf378c4f88407aab
Theodore Moore <theodoremoore618@gmail.com>
2022-05-10 17:59:38 -0400

    Created Lightning Cards to display hero info

--- EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
+++ EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
@@ -1,13 +1,16 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import getHeroInfo from '@salesforce/apex/EmployerPartnerExperienceSiteHelper.getHeroInfo';
 export default class EmployerPartnerExperienceSite extends LightningElement {
-    @wire (getHeroInfo) heroesPre;
-    heroes=[];
-    EmployerPartnerExperienceSite(){
-        for (let i = 0; i < heroesPre.length; i++) {
-            heroes.push(JSON.parse(heroesPre[i]));
+    
+    @track heroes=[];
+    @wire (getHeroInfo)     
+    wiredAccount({ error, data }) {
+        if (data) {
+            for (let i = 0; i < data.length; i++) {
+                this.heroes.push(JSON.parse(data[i]));
+                console.log(JSON.parse(data[i]));
+            }
         }
-        alert(this.heroes);
     }
-
+    
 }
