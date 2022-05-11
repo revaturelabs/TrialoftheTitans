72e35efb4111dd4d8cadb5d703bec60d930418d2
Theodore Moore <theodoremoore618@gmail.com>
2022-05-11 18:23:08 -0400

    Working List with bottom buttons not working

--- EmployerPartnerPackage/main/default/lwc/heroResultsComponent/heroResultsComponent.js
+++ EmployerPartnerPackage/main/default/lwc/heroResultsComponent/heroResultsComponent.js
@@ -1,7 +1,25 @@
+import hero from '@salesforce/resourceUrl/hero';
 import { LightningElement, track , api} from 'lwc';
 
 export default class HeroResultsComponent extends LightningElement {
     //This is being grabbed from employerPartnerExperience
     @api heroes;
+    @track pages={};
+    @track currentPage=1;
+    @track currentHeroes;
+   /* async nextPage(event){
+
+        if((this.currentPage+1)*9<this.heroes.length){
+            this.currentPage= [];
+            
+            let start= 9*(this.currentPage-1);
+            let end = (9*this.currentPage);
+            for (let i = start; i < heroes.length && i< end; i++) {
+                this.currentHeroes.push(this.heroes[i]); 
+            }
+        }
+        this.currentPage=this.currentPage+1;
+        }
+*/
 
 }
