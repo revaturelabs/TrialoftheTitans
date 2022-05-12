d43d7d4e50cdebdf8b7a48b8cb6b6611072fa539
NamPhan56 <namp@umass.edu>
2022-05-12 17:24:37 -0400

    test

---
+++ PortfolioPackage/main/default/lwc/feats_projects/feats_projects.js
@@ -0,0 +1,82 @@
+import { LightningElement, track, wire } from 'lwc';
+//importing all the apex methods from the portfolioProjectHelper class
+import getProjects from '@salesforce/apex/portfolioProjectHelper.getProjects';
+//import getAllProjects from '@salesforce/apex/portfolioProjectHelper.getALLProjects';
+import getResponsibilities from '@salesforce/apex/portfolioProjectHelper.getProjects';
+
+export default class Feats_projects extends LightningElement {
+
+    error;
+    @track exist;
+    @track wirevalue;
+    @track projects;
+
+    //responsibilities
+    @track responsibilitiesWireValue;
+    @track responsibilities;
+
+    activeSectionMessage = '';
+
+    handleToggleSection(event) {
+        this.activeSectionMessage =
+            'Open section name:  ' + event.detail.openSections;
+    }
+
+    // wire function to get the 3 latest projects for a specific user
+    @wire (getProjects)
+    wirevalue(value){
+        const {error,data} = value;
+        console.log('wire fire');
+            if(data){
+                this.projects = data;
+                //Verifying if the record exists.
+                if(data.length<1){ 
+                    this.exist=false;
+                    console.log('no projects available');
+                    console.log(this.projects)
+                }
+                else {
+                    this.exist=true;
+                    this.projects = data;
+                    console.log(this.projects, "mydata");
+                }
+            }
+            else if( error) {
+                console.log('wire fail');
+                this.error=error;
+                console.log(this.error);
+                
+            }
+        this.wirevalue=value;
+    }
+
+    //responsibilities
+    // wire function to get the 3 latest responsibilities for a project
+
+    // @wire (getResponsibilities)
+    // responsibilitiesWireValue(value){
+    //     const {error,data} = value;
+    //     console.log('wire fire');
+    //         if(data){
+    //             this.responsibilities = data;
+    //             //Verifying if the record exists.
+    //             if(data.length<1){ 
+    //                 this.exist=false;
+    //                 console.log('no projects available');
+    //                 console.log(this.responsibilities)
+    //             }
+    //             else {
+    //                 this.exist=true;
+    //                 this.responsibilities = data;
+    //                 console.log(this.responsibilities, "mydata");
+    //             }
+    //         }
+    //         else if( error) {
+    //             console.log('wire fail');
+    //             this.error=error;
+    //             console.log(this.error);
+                
+    //         }
+    //     this.responsibilitiesWireValue=value;
+    // }
+}
