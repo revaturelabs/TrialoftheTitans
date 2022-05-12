acb586726ed5dc05ac898b06810ac939a1fd8d1d
Austin McElrone <81967339+amcelrone@users.noreply.github.com>
2022-05-12 14:13:05 -0700

    Update assignmentCard.js

--- PortfolioPackage/main/default/lwc/assignmentCard/assignmentCard.js
+++ PortfolioPackage/main/default/lwc/assignmentCard/assignmentCard.js
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 =======
 import { LightningElement, wire } from 'lwc';
 >>>>>>> 1ca2866c4065b0e6d75dfd38e8aeb2e3e8ea250d
@@ -13,4 +13,14 @@ export default class assignmentCard extends LightningElement {
 =======
     @wire(getExams) Exams;
 >>>>>>> 1ca2866c4065b0e6d75dfd38e8aeb2e3e8ea250d
+    
+    @track isModalOpen = true;
+    openModal(event) {
+       
+        this.isModalOpen = true;
+    }
+    closeModal(event) {
+        
+        this.isModalOpen = false;
+    }
 }
