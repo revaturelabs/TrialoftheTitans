56ff6c65c6cad902de482615f9dbea19c5fc4c82
Theodore Moore <theodoremoore618@gmail.com>
2022-05-11 21:57:46 -0400

    Completed search result functionality

--- EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
+++ EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
@@ -5,17 +5,15 @@ export default class EmployerPartnerExperienceSite extends LightningElement {
     @track heroes=[];
     @track filteredHeroes;
     @track queryTerm;
-    @track pages={};
-    @track currentPage=1;
-    @track currentHeroes=[];
 
 
     @wire (getHeroInfo)     
     wiredHero({ error, data }) {
         if (data) {
             for (let i = 0; i < data.length; i++) {
+                
                 this.heroes.push(JSON.parse(data[i]));
-                console.log(JSON.parse(data[i]));
+
             }
             this.filteredHeroes= this.heroes;
         }
