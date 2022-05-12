915feb0a547daf18aa2b1bddd366b4d982cebded
Carlos Concepcion <carlosjconcepcion@gmail.com>
2022-05-12 19:20:22 -0400

    CarlosThursday

--- EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
+++ EmployerPartnerPackage/main/default/lwc/employerPartnerExperienceSite/employerPartnerExperienceSite.js
@@ -5,13 +5,9 @@ export default class EmployerPartnerExperienceSite extends LightningElement {
     @track heroes = [];
     @track filteredHeroes;
     @track queryTerm;
-<<<<<<< HEAD
     @track pages = {};
     @track currentPage = 1;
     @track currentHeroes = [];
-=======
-
->>>>>>> 02c22b4efe4eb6343557819edb673f018ed40fcd
 
 
     @wire(getHeroInfo)
@@ -25,11 +21,8 @@ export default class EmployerPartnerExperienceSite extends LightningElement {
             this.filteredHeroes = this.heroes;
         }
     }
-<<<<<<< HEAD
 
     handleFilterEvent(event) {
         this.filteredHeroes = event.detail;
     }
-=======
->>>>>>> 02c22b4efe4eb6343557819edb673f018ed40fcd
 }
