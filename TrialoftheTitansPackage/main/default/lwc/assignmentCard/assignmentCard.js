a1a67a290d75d259ff34edec6995fc794ce319ab
Austin McElrone <81967339+amcelrone@users.noreply.github.com>
2022-05-12 13:04:15 -0700

    Delete assignmentCard.js

--- TrialoftheTitansPackage/main/default/lwc/assignmentCard/assignmentCard.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-
-import getExams from '@salesforce/apex/AssignmentController.getExams';
-
-export default class assignmentCard extends LightningElement {
-    @wire(getExams) Exams;
-}
