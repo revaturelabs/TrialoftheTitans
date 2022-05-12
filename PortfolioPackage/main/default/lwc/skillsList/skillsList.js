03ac5b4f0d406c8bf2446b4456102e63af0f020b
Tim Hinga <timhinga@hotmail.com>
2022-05-12 17:15:16 -0400

    Fix problems

--- PortfolioPackage/main/default/lwc/skillsList/skillsList.js
+++ PortfolioPackage/main/default/lwc/skillsList/skillsList.js
@@ -1,5 +1,4 @@
 import { LightningElement, wire} from 'lwc';
-<<<<<<< HEAD
 import getSkills from '@salesforce/apex/UserInfoHelper.getDonutData'
 
 export default class skillsList extends LightningElement {
@@ -9,14 +8,4 @@ export default class skillsList extends LightningElement {
         let skill = event.target.dataset.id;
         this.dispatchEvent(new CustomEvent('skillclick', {detail: skill}));
     }
-=======
-import getSkills from '@salesforce/apex/getDataForDonut.getDonutData'
-
-export default class FeatsList extends LightningElement {
-    @wire (getSkills) skills;
-
-    /*feats = ['Git', 'APIs', 'Security', 'Triggers', 'Declarative',
-'Object Manager', 'Data Modeling', 'LWC', 'Aura', 'Algorithms']; */
->>>>>>> 1ca2866c4065b0e6d75dfd38e8aeb2e3e8ea250d
-
 }
