edcd6ee687827379960d84b572b7c76d983d96c6
Tim Hinga <timhinga@hotmail.com>
2022-05-13 12:18:33 -0400

    Added comments to skillsList component

--- PortfolioPackage/main/default/lwc/skillsList/skillsList.js
+++ PortfolioPackage/main/default/lwc/skillsList/skillsList.js
@@ -1,9 +1,14 @@
+/* Javascript controller for the list of skills earned on the portfolio page
+    Authors: Tim Hinga, Adam Baird, Alberto Vergara, Austin McElrone
+    Date: May 13, 2022 */
+
 import { LightningElement, wire, track} from 'lwc';
 import getSkills from '@salesforce/apex/UserInfoHelper.getDonutData'
 
 export default class skillsList extends LightningElement {
     @wire (getSkills) skills;
 
+    //Method that gets the skill clicked and dispatches an event with the skill name
     clickEvent(event) {    
         let skill = event.target.dataset.id;
         this.dispatchEvent(new CustomEvent('skillclick', {detail: skill}));
