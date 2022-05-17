5d8e45ac0b691f9d20524c5d9ef44798340b105f
Alfred Do <alfreddowork@gmail.com>
2022-05-17 08:42:47 -0700

    working model uploaded
    * working model
    * added title
    * image works properly

--- PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
+++ PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import mountainStaticImg from '@salesforce/resourceUrl/mountainStaticImg';
+export default class TabSwitchingComponent extends LightningElement {
 
-export default class TabSwitchingComponent extends LightningElement {}
+    
+    mountainUrl = mountainStaticImg;
+}
