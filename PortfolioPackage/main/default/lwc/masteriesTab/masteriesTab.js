b3c48251a216ee936c9ff552d36962910d20127e
Tim Hinga <timhinga@hotmail.com>
2022-05-18 14:13:22 -0400

    Commented masteriesTab JS controller

--- PortfolioPackage/main/default/lwc/masteriesTab/masteriesTab.js
+++ PortfolioPackage/main/default/lwc/masteriesTab/masteriesTab.js
@@ -1,3 +1,8 @@
+/* JS controller for the parent component that holds all components of the Masteries Tab
+    on the portfolio page. Handles events to determine what skill is clicked to load
+    the appropriate modal
+    Authors: Tim Hinga, Adam Baird, Alberto Vergara, Austin McElrone
+    Date: May 18, 2022 */
 import { LightningElement, track } from 'lwc';
 
 export default class MasteriesTab extends LightningElement {
