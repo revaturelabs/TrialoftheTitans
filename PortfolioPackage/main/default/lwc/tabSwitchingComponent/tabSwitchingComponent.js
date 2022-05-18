3c63a0b53053cba751b56b8d1f1081eaf21787c1
Alfred Do <alfreddowork@gmail.com>
2022-05-18 09:58:49 -0700

    WIP animated collaspable
    WIP, needs proper transfer to LWC

--- PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
+++ PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
@@ -4,4 +4,31 @@ export default class TabSwitchingComponent extends LightningElement {
 
     
     mountainUrl = mountainStaticImg;
+
+    connectedCallback(){
+        console.log('HelloWorld');
+
+        // const projectComponent = this.template.querySelector(".projectComponent");
+        // const projectBody = projectComponent.querySelectorAll(".projectBody");
+        
+
+        const projectDisplayComponents = this.template.querySelectorAll('.projectComponent .projectBody .projectDisplayComponent');
+        console.log("projectDisplayComponents: " + projectDisplayComponents);
+        projectDisplayComponents.forEach(projectDisplayComponent =>{
+        projectDisplayComponent.addEventListener('click', event =>{
+        projectDisplayComponent.classList.toggle('active');
+        console.log('clicked');
+        const displayContentContainer = projectDisplayComponent.nextElementSibling;
+        if(projectDisplayComponent.classList.contains('active')){
+          displayContentContainer.style.maxHeight = displayContentContainer.scrollHeight+"px";
+        } else {
+          displayContentContainer.style.maxHeight = 0;
+        }
+      });
+    });
+
+    }
+    
+
 }
+
