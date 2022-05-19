2ec07d7ec8633d5140ef289033f5ba034913abc1
NamPhan56 <namp@umass.edu>
2022-05-19 12:13:18 -0400

    Projects are queried and displayed from database

--- PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
+++ PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
@@ -1,9 +1,48 @@
-import { LightningElement } from 'lwc';
-import mountainStaticImg from '@salesforce/resourceUrl/mountainStaticImg';
+import { LightningElement, track, wire, api } from 'lwc';
+
+import getProjects from "@salesforce/apex/portfolioProjectHelper.getProjects";
+import getResponsibilities from "@salesforce/apex/portfolioProjectHelper.getProjects";
+
+
+//import mountainStaticImg from '@salesforce/resourceUrl/mountainStaticImg';
 export default class TabSwitchingComponent extends LightningElement {
 
     // allows for use of mountain static image
-    mountainUrl = mountainStaticImg;
+    //mountainUrl = mountainStaticImg;
+
+    error;
+    @track wirevalue;
+    @track projects;
+    @api buttonToDisplay;
+
+    //responsibilities
+    @track responsibilitiesWireValue;
+    @track responsibilities;
+
+    // wire function to get the 3 latest projects for a specific user
+    @wire(getProjects)
+    wirevalue(value) {
+        const { error, data } = value;
+        //console.log("wire fire");
+        if (data) {
+            this.projects = data;
+            //Verifying if the record exists.
+            if (data.length < 1) {
+                this.exist = false;
+                //console.log("no projects available");
+                //console.log(this.projects);
+            } else {
+                this.exist = true;
+                this.projects = data;
+                //console.log(this.projects, "mydata");
+            }
+        } else if (error) {
+            //console.log("wire fail");
+            this.error = error;
+            console.log(this.error);
+        }
+        this.wirevalue = value;
+    }
 
     renderedCallback(){
 
