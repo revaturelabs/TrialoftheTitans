c8424aac5042e1d1b7c6bac7eb18b66a17710271
adambaird8 <adamkurtbaird@gmail.com>
2022-05-23 14:24:58 -0400

    dynamic changes

--- PortfolioPackage/main/default/lwc/lwcResponsibilities/lwcResponsibilities.js
+++ PortfolioPackage/main/default/lwc/lwcResponsibilities/lwcResponsibilities.js
@@ -26,7 +26,6 @@ import getResponsibilities from '@salesforce/apex/ResponsibilitiesController.get
 export default class LwcResponsibilities extends LightningElement {
 
     @api getProjectidFromParent;
-    @api projectID;
     @track responsibilities;
     @track filteredResponsibilities;
     @track wirevalue;
@@ -41,7 +40,7 @@ export default class LwcResponsibilities extends LightningElement {
         this.templateOne = this.templateOne ===true ? false : true;
     }
 
-    @wire(getResponsibilities, {projectID: '$getProjectidFromParent'})
+    @wire(getResponsibilities/*, {projectID: '$getProjectidFromParent'}*/)
     wirevalue(value) {
        
         const { error, data } = value;
--- PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
+++ PortfolioPackage/main/default/lwc/tabSwitchingComponent/tabSwitchingComponent.js
@@ -71,4 +71,3 @@ export default class TabSwitchingComponent extends LightningElement {
     
 
 }
-
