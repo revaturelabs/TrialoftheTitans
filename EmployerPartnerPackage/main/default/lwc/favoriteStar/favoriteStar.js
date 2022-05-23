e15a5748cdea654761d9711767c049055f5525d9
dalcala94 <47342981+dalcala94@users.noreply.github.com>
2022-05-23 09:30:45 -0700

    FavoriteStar Documentation
    Added necessary documentation to LWC favoriteStar.
    
    Laid out what I got working and where I left off due to a code freeze.

--- EmployerPartnerPackage/main/default/lwc/favoriteStar/favoriteStar.js
+++ EmployerPartnerPackage/main/default/lwc/favoriteStar/favoriteStar.js
@@ -1,15 +1,27 @@
+/*
+    AUTHOR: Daniel Alcala
+    CREATION: 2022-05-12
+
+    LAST MODIFIED: 2022-05-23
+
+    -LWC for 'Favorite' funtionality, will be a button from 'utility' that depicts a star.
+        Nested in Parent component: heroResultsComponent.html (inside LWC's {currentHeroes} for-loop).
+
+    -JAVASCRIPT logic
+*/
+
 import { LightningElement, track, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class FavoriteStar extends LightningElement {
 
-    //Star/Favorite state will default to an off toggle state (TRACKED, do I need to expose w/ @api so other components can see it?).
+    //Star/Favorite state will default to an off toggle state (TRACKED).
     @track favoriteState = false;
 
-    //Variable that holds Parent object data of type String (Heroe's name).
+    //Variable that holds Parent object data of type Hero. (hero(user) object) CURRENTLY NOT UTILIZED.
     @api currenthero;
 
-    //Method that handles when "Favorite" button icon is selected. (Might need to be exposed w/ @wire or @api)
+    //Method that handles when "Favorite" button icon is selected. ALL LOGIC SHOULD BE HOUSED HERE.
     handleFavoriteClick(){
         this.favoriteState = !this.favoriteState;
 
@@ -24,8 +36,9 @@ export default class FavoriteStar extends LightningElement {
             this.favoriteToast();
         }
        
-    }
 
+    }
+    //Helper success favorite select toast message.
     favoriteToast(){
         const event = new ShowToastEvent({
             title: 'Favorite Added!',
@@ -37,6 +50,7 @@ export default class FavoriteStar extends LightningElement {
         this.dispatchEvent(event);
     }
 
+    //Helper informational favorite de-select toast message.
     unfavoriteToast(){
         const event = new ShowToastEvent({
             title: 'Favorite Removed!',
