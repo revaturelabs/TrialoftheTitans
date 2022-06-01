02b112985c2989ca40258298b0e624dd29dc163c
Rhys <lev.k.kim@gmail.com>
2022-06-01 17:21:07 -0400

    start project controller and responsibility

---
+++ TitanHubPackage/main/default/lwc/projectResponsiblity/projectResponsiblity.js
@@ -0,0 +1,16 @@
+// Name: Project Responsibility
+// Author: Rhys Kim
+// Created: 6/1/2022
+// Updated: TBA
+// Description: Responsibility section in Project Submission page
+
+import getProjectInfo from '@salesforce/apex/titanPagePortDetailsHelper.getProjectInfo';
+import { LightningElement, api, wire } from 'lwc';
+
+export default class ProjectResponsiblity extends LightningElement {
+
+  hasResponsibility;
+
+
+
+}
