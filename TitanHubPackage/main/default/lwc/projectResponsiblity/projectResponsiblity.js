1811dadebd3f9966f1ec15efbee97640b986c4ec
Rhys <lev.k.kim@gmail.com>
2022-06-02 11:26:49 -0400

    set up structure of responsibility component

--- TitanHubPackage/main/default/lwc/projectResponsiblity/projectResponsiblity.js
+++ TitanHubPackage/main/default/lwc/projectResponsiblity/projectResponsiblity.js
@@ -4,13 +4,37 @@
 // Updated: TBA
 // Description: Responsibility section in Project Submission page
 
-import getProjectInfo from '@salesforce/apex/titanPagePortDetailsHelper.getProjectInfo';
-import { LightningElement, api, wire } from 'lwc';
+import getResponsibilities from '@salesforce/apex/ProjectController.getResponsibilities';
+import { LightningElement, track, api, wire } from 'lwc';
 
 export default class ProjectResponsiblity extends LightningElement {
 
-  hasResponsibility;
+  hasResponsibility = false;
+  // fetch information here
+  @wire(getResponsibilities, {})
+  // dummy method atm until i get some help for this
+  fetchResponsibilities({error, data}) {
+    if (data) {
+      console.log(data);
+    } else {
+      console.log(error);
+    }
+  }
 
+  // modal code
+  @track isModalOpen = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+  
+  confirmDelete() {
+    this.isModalOpen = false;
+    // responsibility should be destroyed here
+  }
 
 }
