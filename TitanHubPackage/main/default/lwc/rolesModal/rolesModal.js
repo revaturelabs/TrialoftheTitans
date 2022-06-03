7fae0c0e422382b751fbd21ecbada8ca10f33bc2
kstetz333 <102083579+kstetz333@users.noreply.github.com>
2022-06-03 12:15:17 -0400

    changes to responsitbility and updates (#520)
    * Some functionality and updates
    
    * updates

--- TitanHubPackage/main/default/lwc/rolesModal/rolesModal.js
+++ TitanHubPackage/main/default/lwc/rolesModal/rolesModal.js
@@ -1,7 +1,9 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import getRole from '@salesforce/apex/ProjectController.getRole';
 const CSS_CLASS = "modal-hidden";
 export default class RolesModal extends LightningElement {
     showModal = false;
+    role;
 
     @api show(){
         this.showModal = true;
@@ -11,5 +13,15 @@ export default class RolesModal extends LightningElement {
     }
     addResponsibility(){
         
+    }
+    projectId = 'a0L7z0000004JaLEAU';
+    @wire(getRole, {projectId: '$projectId'})
+    fetchRole({error, data}){
+        if(data){
+            this.role = data.Roles_Responsibilities__c.slice(3,-4);
+        }
+        else if(error){
+            console.error(error);
+        }
     }
 }
