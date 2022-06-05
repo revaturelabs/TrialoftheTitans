0d9a2061bb0f68f0e8785cc9430388d4957f0357
RD Galang <100622822+rdylang@users.noreply.github.com>
2022-06-05 01:12:59 -0400

    update lwcResponsibilities These are mostly just cosmetic changes, I think. I originally thought the component wasn't working, which is why I made changes to the method handling the wire adapter, but I actually hadn't given added access to the server-side controller on the Hero profile, so I'm pretty sure it was working fine.

--- PortfolioPackage/main/default/lwc/lwcResponsibilities/lwcResponsibilities.js
+++ PortfolioPackage/main/default/lwc/lwcResponsibilities/lwcResponsibilities.js
@@ -1,69 +1,22 @@
-//import { LightningElement, track, wire } from "lwc";
-//import getSomeAccounts from "@salesforce/apex/ResponsibilitiesController.getSomeAccounts";
-//export default class LwcResponsibilities extends LightningElement {
- //   @track data
- //   @track columns = [
-       // { label: 'Responsibilities', fieldName: 'Name', type: 'text'},
-       // { label: 'Label', fieldName: 'Trainer__r.Name', type: 'text'},
-        // {label: 'Label', fieldName: 'Name', type: 'text'},
-        //{ label: 'Phone', fieldName: 'Phone', type: 'phone'},
-       // { label: 'Industry', fieldName: 'Industry', type: 'text'},
-    //];
-   // @wire (getSomeAccounts) accountRecords({error,data}){
-      //  if(data){
-      //      this.data = data;
-      //  }
-      //  else if (error){
-     //       this.data = undefined;
-     //   }
-  //  }
-//}
-
 import { LightningElement, track, wire, api} from 'lwc';
 import viewList from './lwcResponsibilities.html';
 import getResponsibilities from '@salesforce/apex/ResponsibilitiesController.getResponsibilities';
 
 export default class LwcResponsibilities extends LightningElement {
 
-    @api getProjectidFromParent;
-    @track responsibilities;
-    @track filteredResponsibilities;
-    @track wirevalue;
-    filtered = false;
+    @api projectId;
+    responsibilities;
+    filteredResponsibilities;
 
-    templateOne = false;
-    render(){
-        return this.templateOne === false? viewList:viewList;
-    }
-
-    click(){
-        this.templateOne = this.templateOne ===true ? false : true;
-    }
-
-    @wire(getResponsibilities, {projectID: '$getProjectidFromParent'})
-    wirevalue(value) {
-       
-        const { error, data } = value;
-        console.log("wire fire");
+    @wire(getResponsibilities, {projectID: '$projectId'})
+    fetchResponsibilities({error, data}) {
         if (data) {
+            console.log('This is working');
             this.responsibilities = data;
-            //Verifying if the record exists.
-            if (data.length < 1) {
-                console.log("no responsibilities available");
-                console.log(this.responsibilities);
-            } else {
-                this.responsibilities = data;
-                console.log(this.responsibilities, "mydata");
-            }
-        } else if (error) {
-            console.log("wire fail");
-            this.error = error;
-            console.log(this.error);
+            this.filteredResponsibilities = [...this.responsibilities];
+        } else if (error) { 
+            console.log('This is not working');
+            console.error(error);
         }
-    }
-
-    //handler for filtered display
-    //removes any responsibilityIDs that are not selected
-    //sets filtered to true;
-    
+    }    
 }
