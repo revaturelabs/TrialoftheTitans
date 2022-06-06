f831a50bb029c3b38a555673d0509fed53e66a0b
Kyle Stetz <kstetz3@live.com>
2022-06-06 14:26:30 -0400

    test

--- TitanHubPackage/main/default/lwc/rolesModal/rolesModal.js
+++ TitanHubPackage/main/default/lwc/rolesModal/rolesModal.js
@@ -1,21 +1,48 @@
-import { api, LightningElement, wire } from 'lwc';
+import { api, LightningElement, wire, track } from 'lwc';
 import getRole from '@salesforce/apex/ProjectController.getRole';
 //import getProjectInfo from "@salesforce/apex/UserStoryController.getProjectInfo";
 import projectOverview from '@salesforce/messageChannel/projectOverview__c';
 import { subscribe, MessageContext } from 'lightning/messageService'
+import getProjectInfo from "@salesforce/apex/UserStoryController.getProjectInfo";
 const CSS_CLASS = "modal-hidden";
 export default class RolesModal extends LightningElement {
-    showModal = false;
+    
     role;
     @wire(MessageContext)
     context;
-    @api projectId;
+    projectId;
     @api titanId;
+
+
+    //modal controls
+    showModal = false;
+    @track isModalOpen = false;
     @api show(){
         this.showModal = true;
     }
     handleDialogClose(){
         this.showModal = false;
+    }
+    openModal() {
+        this.isModalOpen = true;
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+    }
+    confirmDelete() {
+        this.isModalOpen = false;
+        // responsibility should be destroyed here
+    }
+    //get project info and Id
+    @wire(getProjectInfo,{titanId: '$titanId'})
+    getProject({error, data}) {
+        if (data) {
+            this.projectId = data.Id;
+        }
+        else if (error) {
+            console.error(error);
+        }
     }
     addResponsibility(){
         
@@ -29,14 +56,20 @@ export default class RolesModal extends LightningElement {
     handleMessage(message) {
         
         this.projectId = message.projectId;
+        console.log('test');
     }
     @wire(getRole, {projectId: '$projectId'})
     fetchRole({error, data}){
         if(data){
-            this.role = data.Roles_Responsibilities__c.slice(3,-4);
+            this.role = data.Role__c.slice(3,-4);
         }
         else if(error){
             console.error(error);
         }
     }
+    
+
+    
+    
+    
 }
